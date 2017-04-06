0fbf67c5cce038535a41777d0be14cc9c01b7812
Christopher Friedt <cfri@pervices.com>
2017-04-06 17:08:56 +0000

    add tsi_type and tsf_type field handling with unit test.
    The unit test is
    
    uhd/host/tests/vrt_test.cpp::test_with_tsi_other_tsf_pico()

--- host/tests/vrt_test.cpp
+++ host/tests/vrt_test.cpp
@@ -53,6 +53,7 @@ static void pack_and_unpack(
     );
 
     //check the the unpacked metadata is the same
+    BOOST_CHECK_EQUAL(if_packet_info_in.packet_type, if_packet_info_out.packet_type);
     BOOST_CHECK_EQUAL(if_packet_info_in.packet_count, if_packet_info_out.packet_count);
     BOOST_CHECK_EQUAL(if_packet_info_in.num_header_words32, if_packet_info_out.num_header_words32);
     BOOST_CHECK_EQUAL(if_packet_info_in.num_payload_words32, if_packet_info_out.num_payload_words32);
@@ -64,10 +65,12 @@ static void pack_and_unpack(
     if (if_packet_info_in.has_cid and if_packet_info_out.has_cid){
         BOOST_CHECK_EQUAL(if_packet_info_in.cid, if_packet_info_out.cid);
     }
+    BOOST_CHECK_EQUAL(if_packet_info_in.tsi_type, if_packet_info_out.tsi_type);
     BOOST_CHECK_EQUAL(if_packet_info_in.has_tsi, if_packet_info_out.has_tsi);
     if (if_packet_info_in.has_tsi and if_packet_info_out.has_tsi){
         BOOST_CHECK_EQUAL(if_packet_info_in.tsi, if_packet_info_out.tsi);
     }
+    BOOST_CHECK_EQUAL(if_packet_info_in.tsf_type, if_packet_info_out.tsf_type);
     BOOST_CHECK_EQUAL(if_packet_info_in.has_tsf, if_packet_info_out.has_tsf);
     if (if_packet_info_in.has_tsf and if_packet_info_out.has_tsf){
         BOOST_CHECK_EQUAL(if_packet_info_in.tsf, if_packet_info_out.tsf);
@@ -180,5 +183,22 @@ BOOST_AUTO_TEST_CASE(test_with_chdr){
     if_packet_info.tsi = std::rand();
     if_packet_info.tsf = std::rand();
     if_packet_info.num_payload_words32 = 24;
+    pack_and_unpack(if_packet_info);
+}
+
+BOOST_AUTO_TEST_CASE(test_with_tsi_other_tsf_pico){
+    vrt::if_packet_info_t if_packet_info;
+    if_packet_info.link_type = vrt::if_packet_info_t::LINK_TYPE_NONE;
+    if_packet_info.packet_count = 0;
+    if_packet_info.has_sid = false;
+    if_packet_info.has_cid = false;
+    if_packet_info.has_tsi = true;
+    if_packet_info.tsi_type = vrt::if_packet_info_t::tsi_type_t::TSI_TYPE_OTHER;
+    if_packet_info.has_tsf = true;
+    if_packet_info.tsf_type = vrt::if_packet_info_t::tsf_type_t::TSF_TYPE_PICO;
+    if_packet_info.has_tlr = false; //tlr not suported in CHDR
+    if_packet_info.tsi = std::rand();
+    if_packet_info.tsf = std::rand();
+    if_packet_info.num_payload_words32 = 0;
     pack_and_unpack(if_packet_info);
 }
