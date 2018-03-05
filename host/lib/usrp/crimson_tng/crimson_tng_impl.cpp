c9897ed8b2a0565f8eaef99756885cdf1ed266c1
Christopher Friedt <cfri@pervices.com>
2018-03-05 20:49:04 +0000

    tick rate was not being updated because power was off

--- host/lib/usrp/crimson_tng/crimson_tng_impl.cpp
+++ host/lib/usrp/crimson_tng/crimson_tng_impl.cpp
@@ -1302,14 +1302,12 @@ crimson_tng_impl::~crimson_tng_impl(void)
 
 		for( size_t i = 0; i < mbc.rx_streamers.size(); i++ ) {
 			uhd::rx_streamer::sptr sptr = mbc.rx_streamers[ i ].lock();
-			delete sptr.get();
 			sptr.reset();
 		}
 		mbc.rx_streamers.clear();
 
 		for( size_t i = 0; i < mbc.tx_streamers.size(); i++ ) {
 			uhd::tx_streamer::sptr sptr = mbc.tx_streamers[ i ].lock();
-			delete sptr.get();
 			sptr.reset();
 		}
 		mbc.tx_streamers.clear();
