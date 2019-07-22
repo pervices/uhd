4ed82166587268ec4319a38c637674ca3946daeb
Amir Ghanbari Bavarsad <amir.b@pervices.com>
2019-07-22 19:33:12 +0000

    TATE,VAUNT: Update example for stacked gpio commands

--- host/examples/test_stacked_gpio_commands.cpp
+++ host/examples/test_stacked_gpio_commands.cpp
@@ -1,16 +1,21 @@
 #include <uhd/utils/thread.hpp>
 #include <uhd/utils/safe_main.hpp>
 #include <uhd/usrp/multi_usrp.hpp>
-
 namespace gpio
 {
-    void write(uhd::usrp::multi_usrp::sptr& usrp, const uint64_t pins, const uint64_t mask, const double time)
+    void write(uhd::usrp::multi_usrp::sptr& usrp, const uint64_t pins [], const uint64_t mask [], const double time)
     {
         usrp->set_command_time(uhd::time_spec_t(time));
-        usrp->set_user_register(0, (uint32_t) (pins >> 0x00)); // Lower first.
-        usrp->set_user_register(1, (uint32_t) (pins >> 0x20)); // Then upper.
-        usrp->set_user_register(2, (uint32_t) (mask >> 0x00)); // Same goes for the mask.
-        usrp->set_user_register(3, (uint32_t) (mask >> 0x20));
+        usrp->set_user_register(0, (uint32_t) (pins[0] >> 0x00)); // Lower first.
+        usrp->set_user_register(1, (uint32_t) (mask[0] >> 0x00)); // Same goes for the mask.
+        usrp->set_user_register(2, (uint32_t) (pins[0] >> 0x20)); // Then upper.
+        usrp->set_user_register(3, (uint32_t) (mask[0] >> 0x20));
+#ifdef PV_TATE
+        usrp->set_user_register(4, (uint32_t) (pins[1] >> 0x00)); // Lower first.
+        usrp->set_user_register(5, (uint32_t) (mask[1] >> 0x00)); // Same goes for the mask.
+        usrp->set_user_register(6, (uint32_t) (pins[1] >> 0x20)); // Then upper.
+        usrp->set_user_register(7, (uint32_t) (mask[1] >> 0x20));
+#endif
     }
 }
 
@@ -25,10 +30,21 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 
     usrp->set_time_now(uhd::time_spec_t(0.0));
 
+#ifdef PV_TATE
+    uint64_t pins [2] = {0x0, 0x0};
+    uint64_t mask [2] = {0x0, 0x0};
+    const uint64_t all = 0xFFFFFFFFFFFFFFFF;
+    for(double time = 0.0; time < 64.0; time++) {
+        pins[0] ^= all;
+        pins[1] ^= all;
+        gpio::write(usrp, pins , mask, time);
+    }
+#else
     uint64_t pins = 0x0;
     const uint64_t all = 0xFFFFFFFFFF;
     for(double time = 0.0; time < 64.0; time++)
         gpio::write(usrp, pins ^= all, all, time);
+#endif
 
     return 0;
 }
