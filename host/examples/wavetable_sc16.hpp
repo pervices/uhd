260a74af92d7ab6f1585dae84e36c5e1dad90c2c
Amir Ghanbari Bavarsad <amir.b@pervices.com>
2020-01-20 17:59:11 +0000

    Fix Endianness of UC16 wavetable generator

--- host/examples/wavetable_sc16.hpp
+++ host/examples/wavetable_sc16.hpp
@@ -19,7 +19,7 @@ public:
         _wave_table(wave_table_len)
     {
         //compute real wave table with 1.0 amplitude
-        std::vector<uint16_t> real_wave_table(wave_table_len);
+        std::vector<float> real_wave_table(wave_table_len);
         if (wave_type == "CONST"){
             for (size_t i = 0; i < wave_table_len; i++)
                 real_wave_table[i] = 1;
@@ -34,27 +34,32 @@ public:
         }
         else if (wave_type == "SINE"){
             static const double tau = 2*std::acos(-1.0);
+            double scale_factor = 32767.;
             for (size_t i = 0; i < wave_table_len; i++)
-                real_wave_table[i] = std::sin((tau*i)/wave_table_len);
+                real_wave_table[i] = std::sin((tau*i)/wave_table_len) * scale_factor * ampl;
         }
         else throw std::runtime_error("unknown waveform type: " + wave_type);
 
-        //compute i and q pairs with 90% offset and scale to amplitude
+        // compute i and q pairs with 90% offset and scale to amplitude
         for (size_t i = 0; i < wave_table_len; i++){
             const size_t q = (i+(3*wave_table_len)/4)%wave_table_len;
-            _wave_table[i] = std::complex<uint16_t>(
-                ampl*real_wave_table[i],
-                ampl*real_wave_table[q]
-            );
+
+            uint32_t real = int16_t(real_wave_table[i]);
+            uint32_t imag = int16_t(real_wave_table[q]);
+
+            uint32_t val = (real << 16) + imag;
+            // Byte swap
+            val = ((val & 0xff000000) >> 24 ) | ((val & 0x00ff0000) >> 8 ) | ((val & 0x0000ff00) << 8 ) | ((val & 0x000000ff) << 24 );
+            _wave_table[i] = val;
         }
     }
 
     inline uint32_t operator()(const size_t index) const{
-        return ((_wave_table[index % wave_table_len].real() << 16) | _wave_table[index % wave_table_len].imag() << 0);
+        return _wave_table[index % wave_table_len];
     }
 
 private:
-    std::vector<std::complex<uint16_t> > _wave_table;
+    std::vector<uint32_t> _wave_table;
 };
 
 
