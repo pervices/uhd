0719a02dafd641a5f75d99f6c4f119b3f27ba557
Amir Ghanbari Bavarsad <amir.b@pervices.com>
2020-03-25 20:03:31 +0000

    TATE: WIP Initial value for tune_request_t in constructor

--- host/lib/types/tune.cpp
+++ host/lib/types/tune.cpp
@@ -15,6 +15,9 @@ tune_request_t::tune_request_t(double target_freq):
     target_freq(target_freq),
     rf_freq_policy(POLICY_AUTO),
     rf_freq(0.0),
+    rf_ch_freq(0.0),
+    rf_dp_freq(0.0),
+    lo_freq(0.0),
     dsp_freq_policy(POLICY_AUTO),
     dsp_freq(0.0)
 {
@@ -25,8 +28,24 @@ tune_request_t::tune_request_t(double target_freq, double lo_off):
     target_freq(target_freq),
     rf_freq_policy(POLICY_MANUAL),
     rf_freq(target_freq + lo_off),
+    rf_ch_freq(0.0),
+    rf_dp_freq(0.0),
+    lo_freq(0.0),
     dsp_freq_policy(POLICY_AUTO),
     dsp_freq(0.0)
+{
+    /* NOP */
+}
+
+tune_request_t::tune_request_t(double target_freq, double rf_ch_freq , double rf_dp_freq, double dsp_freq double lo_off, double dsp_freq ):
+    target_freq(target_freq),
+    rf_freq_policy(POLICY_MANUAL),
+    rf_freq(rf_ch_freq + rf_dp_freq),
+    rf_ch_freq(rf_ch_freq),
+    rf_dp_freq(rf_dp_freq),
+    lo_freq(lo_off),
+    dsp_freq_policy(POLICY_MANUAL),
+    dsp_freq(dsp_freq)
 {
     /* NOP */
 }
