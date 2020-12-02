68c0d8cf5084996e70521684473eda717711044d
Vikram Venkataramanan <vikram.v@pervices.com>
2020-12-02 17:31:56 +0000

    add uhd_usrp_info.cpp

---
+++ host/utils/uhd_usrp_info.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <cstdlib>
+
+#include <boost/program_options.hpp>
+
+#include <uhd/version.hpp>
+#include <uhd/device.hpp>
+#include <uhd/property_tree.hpp>
+
+#include <uhd/utils/safe_main.hpp>
+
+namespace po = boost::program_options;
+
+namespace {
+    //! Conditionally append find_all=1 if the key isn't there yet
+    uhd::device_addr_t append_findall(const uhd::device_addr_t& device_args)
+    {
+        uhd::device_addr_t new_device_args(device_args);
+        if (!new_device_args.has_key("find_all")) {
+            new_device_args["find_all"] = "1";
+        }
+
+        return new_device_args;
+    }
+}
+
+typedef std::map<std::string, std::set<std::string>> device_multi_addrs_t;
+typedef std::map<std::string, device_multi_addrs_t> device_addrs_filtered_t;
+
+void print_usage(po::options_description desc)
+{
+    std::cout << "Usage: uhd_usrp_init [OPTIONS]..." << std::endl
+              << "Diagnostic script that completely captures the state, code, and revision of a Per Vices SDR" << std::endl
+              << std::endl
+              << desc << std::endl
+              << std::endl
+              << "Examples:" << std::endl
+              << "    uhd_usrp_init -v" << std::endl
+              << std::endl;
+}
+
+std::string get_from_tree(uhd::property_tree::sptr tree, const int device_id, const char *relative_path)
+{
+    std::string path = "/mboards/" + std::to_string(device_id) + "/" + relative_path;
+    return tree->access<std::string>(path).get();
+}
+
+device_addrs_filtered_t find_devices(uhd::device_addrs_t device_addrs)
+{
+    device_addrs_filtered_t found_devices;
+    for (auto it = device_addrs.begin(); it != device_addrs.end(); ++it) {
+        std::string serial    = (*it)["serial"];
+        found_devices[serial] = device_multi_addrs_t();
+        for (std::string key : it->keys()) {
+            if (key != "serial") {
+                found_devices[serial][key].insert(it->get(key));
+            }
+        }
+        for (auto sit = it + 1; sit != device_addrs.end();) {
+            if ((*sit)["serial"] == serial) {
+                for (std::string key : sit->keys()) {
+                    if (key != "serial") {
+                        found_devices[serial][key].insert(sit->get(key));
+                    }
+                }
+                sit = device_addrs.erase(sit);
+            } else {
+                sit++;
+            }
+        }
+    }
+    return found_devices;
+}
+
+
+int UHD_SAFE_MAIN(int argc, char* argv[])
+{
+    //uhd::set_thread_priority_safe();
+    po::options_description desc("Allowed options");
+
+    // clang-format off
+    desc.add_options()
+        ("networking,i", "provide the Management/SFP+ port IP addresses")
+        ("time,t", "provide the SOC: 'date' 'hwclock' and the time on SDR")
+        ("lock,l", "provide the PLL lock status for Rx/Tx/time and if we are using internal/external reference")
+        ("boards,r", "provide the RFE front end status for each board")
+        ("all,v", "prints all of the information described above.")
+	("args", po::value<std::string>()->default_value(""), "device address args")
+    ;
+    // clang-format on
+
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    const uhd::device_addr_t args(vm["args"].as<std::string>());
+
+    if (argc <= 1) {
+        print_usage(desc);
+        return EXIT_SUCCESS;
+    }
+
+    std::cout << "UHD Software Library : " << uhd::get_version_string() << std::endl;
+
+    // Find the devices
+    uhd::device_addrs_t device_addrs = uhd::device::find(append_findall(args));
+    if (device_addrs.empty()) {
+        std::cerr << "No UHD Devices Found" << std::endl;
+        return EXIT_FAILURE;
+    }
+    device_addrs_filtered_t found_devices = find_devices(device_addrs);
+
+    int i = 0;
+    int ndevices = found_devices.size();
+
+    std::cout << std::endl;
+    std::cout << ndevices
+	      << " SDRs found!"
+	      << std::endl
+	      << std::endl;
+
+    // create property tree for later
+    uhd::device::sptr dev         = uhd::device::make(vm["args"].as<std::string>());
+    uhd::property_tree::sptr tree = dev->get_tree();
+
+    std::string device_type, device_address;
+    for (auto dit = found_devices.begin(); dit != found_devices.end(); ++dit) {
+	std::cout << "Device " << (i + 1) << std::endl; 
+	
+        for (auto mit = dit->second.begin(); mit != dit->second.end(); ++mit) {
+            for (auto vit = mit->second.begin(); vit != mit->second.end(); ++vit) {
+		if(mit->first == "addr"){
+			device_address = *vit;
+		}
+		if(mit->first == "type"){
+			device_type = *vit;
+		}
+            }
+        }
+
+	std::cout << "Device Type    : " << device_type << std::endl; 
+        std::cout << "TODO: print the server revision\n";
+        std::cout << "FPGA Version   : " << get_from_tree(tree, i, "fw_version") << std::endl;
+
+        std::cout << "TODO: print the uhd revision for all the plugged in boards\n";
+	std::cout << "Board Hardware Versions : " << std::endl;
+        std::cout << "\tTime 0: " << get_from_tree(tree, i, "time/fw_version") << std::endl;
+        std::cout << "\tTx   0: " << get_from_tree(tree, i, "tx/fw_version") << std::endl;
+        std::cout << "\tRx   0: " << get_from_tree(tree, i, "rx/fw_version") << std::endl;
+
+        if (vm.count("networking") || vm.count("all") ) {
+	    std::cout << "Device Address : " << std::endl;
+	    std::cout << "\tManagement IP: " << device_address << std::endl; 
+            std::cout << "\tSFP A IP     : " << get_from_tree(tree, i, "sfpa/ip_addr") << std::endl;
+            std::cout << "\tSFP B IP     : " << get_from_tree(tree, i, "sfpb/ip_addr") << std::endl;
+        }
+
+        if (vm.count("time") || vm.count("all") ) {
+            //std::cout << "Time (fpga/board/gps): " << get_from_tree(tree, i, "gps_time") << std::endl;
+            std::cout << "TODO: provide the SOC: 'date' 'hwclock' and the time on SDR\n";
+        }
+
+        if (vm.count("lock") || vm.count("all") ) {
+            std::cout << "TODO: provide the PLL lock status for Rx/Tx/time and if we are using internal/external reference\n";
+        }
+
+        if (vm.count("boards") || vm.count("all") ) {
+            std::cout << "TODO: RFE frontend status of each board\n";
+        }
+
+        i++;
+    }
+
+    std::cout << std::endl;
+
+    return EXIT_SUCCESS;
+}
