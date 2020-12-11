f62295906203d8abec40904e21bd60ecdda24b43
Vikram Venkataramanan <vikram.v@pervices.com>
2020-12-11 16:14:08 +0000

    try add board info

--- host/lib/usrp/crimson_tng/crimson_tng_impl.cpp
+++ host/lib/usrp/crimson_tng/crimson_tng_impl.cpp
@@ -1006,6 +1006,12 @@ crimson_tng_impl::crimson_tng_impl(const device_addr_t &_device_addr)
         TREE_CREATE_RW(rx_path / dspno / "/trigger/ufl_dir"        , "rx_" + lc_num + "/trigger/ufl_dir"        , std::string, string);
         TREE_CREATE_RW(rx_path / dspno / "/trigger/ufl_pol"        , "rx_" + lc_num + "/trigger/ufl_pol"        , std::string, string);
 
+
+		// About information
+		TREE_CREATE_RW(rx_path / dspno / "fw_ver", "rx_"+lc_num+"/about/fw_ver", std::string, string);
+		TREE_CREATE_RW(rx_path / dspno / "hw_ver", "rx_"+lc_num+"/about/hw_ver", std::string, string);
+		TREE_CREATE_RW(rx_path / dspno / "sw_ver", "rx_"+lc_num+"/about/sw_ver", std::string, string);
+
 		// Power status
 		TREE_CREATE_RW(rx_path / dspno / "pwr", "rx_"+lc_num+"/pwr", std::string, string);
 
@@ -1145,6 +1151,11 @@ crimson_tng_impl::crimson_tng_impl(const device_addr_t &_device_addr)
         TREE_CREATE_RW(tx_path / dspno / "/trigger/ufl_dir"        , "tx_" + lc_num + "/trigger/ufl_dir"        , std::string, string);
         TREE_CREATE_RW(tx_path / dspno / "/trigger/ufl_pol"        , "tx_" + lc_num + "/trigger/ufl_pol"        , std::string, string);
         TREE_CREATE_RW(tx_path / dspno / "/trigger/gating"         , "tx_" + lc_num + "/trigger/gating"         , std::string, string);
+
+		// About information
+		TREE_CREATE_RW(tx_path / dspno / "fw_ver", "tx_"+lc_num+"/about/fw_ver", std::string, string);
+		TREE_CREATE_RW(tx_path / dspno / "hw_ver", "tx_"+lc_num+"/about/hw_ver", std::string, string);
+		TREE_CREATE_RW(tx_path / dspno / "sw_ver", "tx_"+lc_num+"/about/sw_ver", std::string, string);
 
 		// Actual frequency values
 		TREE_CREATE_RW(tx_path / chan / "freq" / "value", "tx_"+lc_num+"/rf/freq/val", double, double);
