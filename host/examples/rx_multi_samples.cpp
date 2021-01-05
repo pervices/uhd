7896467fc3d0ebb7166c355c56a3c3fb55bd7720
Vikram Venkataranan <vikram.venkataramanan@mail.utoronto.ca>
2021-01-05 16:30:11 -0500

    spelling

--- host/examples/rx_multi_samples.cpp
+++ host/examples/rx_multi_samples.cpp
@@ -213,7 +213,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
         for (size_t i = 0; i < ofstream_ptrs.size(); i++) {
             if (ofstream_ptrs.at(i)->is_open()) {
                 std::cout << std::to_string(i) << "\n";
-                ofstream_ptrs.at(i)->write( (const char*)&(buff.at(i).front()), channel_buff_size );
+                ofstream_ptrs.at(i)->write( (const char*)&(buffs.at(i).front()), channel_buff_size );
             }
         }
 
