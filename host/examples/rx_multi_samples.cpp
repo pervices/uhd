839d3b41884e7f766f0567ea3864a2a25291c662
Vikram Venkataranan <vikram.venkataramanan@mail.utoronto.ca>
2021-01-05 13:27:00 -0500

    try iterators

--- host/examples/rx_multi_samples.cpp
+++ host/examples/rx_multi_samples.cpp
@@ -189,11 +189,16 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
 
         num_acc_samps += num_rx_samps;
 
-        for (size_t i = 0; i < ofstream_ptrs.size(); i++) {
-            if (ofstream_ptrs.at(i)->is_open()) {
-                /* ofstream_ptrs.at(i)->write( (const char*)&(buff_ptrs[i]), num_rx_samps * sizeof(std::complex<float>) ); */
+        for (auto &it: ofstream_ptrs) {
+            if(it->is_open()){
             }
         }
+
+        /* /1* for (size_t i = 0; i < ofstream_ptrs.size(); i++) { *1/ */
+        /*     if (ofstream_ptrs.at(i)->is_open()) { */
+                /* ofstream_ptrs.at(i)->write( (const char*)&(buff_ptrs[i]), num_rx_samps * sizeof(std::complex<float>) ); */
+        /*     } */
+        /* } */
         std::cout << "Made it here 2 \n";
 
     }
