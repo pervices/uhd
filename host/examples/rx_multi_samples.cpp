a2e3dc485735a275f9a6924b53071f7d8ddccfa0
Vikram Venkataranan <vikram.venkataramanan@mail.utoronto.ca>
2021-01-05 13:39:29 -0500

    change std::string to std::to_string

--- host/examples/rx_multi_samples.cpp
+++ host/examples/rx_multi_samples.cpp
@@ -192,7 +192,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
 
         for (size_t i = 0; i < ofstream_ptrs.size(); i++) {
             if (ofstream_ptrs.at(i)->is_open()) {
-                std::cout << std::string(i) << "\n";
+                std::cout << std::to_string(i) << "\n";
                 ofstream_ptrs.at(i)->write( (const char*)buff_ptrs[i], num_rx_samps * sizeof(std::complex<float>) );
             }
         }
