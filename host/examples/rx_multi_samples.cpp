da01f67dcfc9a121674e2e5aaebadaccc53c5a0a
Vikram Venkataranan <vikram.venkataramanan@mail.utoronto.ca>
2021-01-05 13:08:34 -0500

    where did the s go?

--- host/examples/rx_multi_samples.cpp
+++ host/examples/rx_multi_samples.cpp
@@ -191,7 +191,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
 
         for (size_t i = 0; i < ofstream_ptrs.size(); i++) {
             if (ofstream_ptrs.at(i)->is_open()) {
-                ofstream_ptr-.at(i)>write( (const char*)&(buff_ptrs[i]), num_rx_samps * sizeof(std::complex<float>) );
+                ofstream_ptrs.at(i)>write( (const char*)&(buff_ptrs[i]), num_rx_samps * sizeof(std::complex<float>) );
             }
         }
         std::cout << "Made it here 2 \n";
