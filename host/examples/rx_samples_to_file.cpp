8efec8b6a304ed301f4c374da500d45f2d21b91a
Doug W <doug.w@pervices.com>
2021-06-22 15:55:12 +0000

    DEBUG: checking if only rx_stream will result in packets

--- host/examples/rx_samples_to_file.cpp
+++ host/examples/rx_samples_to_file.cpp
@@ -65,9 +65,9 @@ template<typename samp_type> void recv_to_file(
     stream_cmd.stream_now = true;
     stream_cmd.time_spec = uhd::time_spec_t();
 
-    std::cout << "Commenting out issuing stream command" << std::endl;
-    //rx_stream->issue_stream_cmd(stream_cmd);
-
+    //std::cout << "Commenting out issuing stream command" << std::endl;
+    std::cout << "Checking if only this command will make it run" << std::endl
+    rx_stream->issue_stream_cmd(stream_cmd);
     std::exit(0);
 
     typedef std::map<size_t,size_t> SizeMap;
