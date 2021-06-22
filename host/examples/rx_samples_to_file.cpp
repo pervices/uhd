6f036a6f080f82ba9b13884fa218a209355639d8
Doug W <doug.w@pervices.com>
2021-06-22 15:19:54 +0000

    DEBUG: looking where the start streaming comand is called

--- host/examples/rx_samples_to_file.cpp
+++ host/examples/rx_samples_to_file.cpp
@@ -63,7 +63,8 @@ template<typename samp_type> void recv_to_file(
     stream_cmd.num_samps = size_t(num_requested_samples);
     stream_cmd.stream_now = true;
     stream_cmd.time_spec = uhd::time_spec_t();
-    rx_stream->issue_stream_cmd(stream_cmd);
+    std::cout "Issuing stream command is commented out" std::endl;
+    //rx_stream->issue_stream_cmd(stream_cmd);
 
     typedef std::map<size_t,size_t> SizeMap;
     SizeMap mapSizes;
