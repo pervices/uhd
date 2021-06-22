2d033ea4436e795a5a9e633ac028d628797593f0
Doug W <doug.w@pervices.com>
2021-06-22 15:41:51 +0000

    DEBUG: fixed previous commit

--- host/examples/rx_samples_to_file.cpp
+++ host/examples/rx_samples_to_file.cpp
@@ -64,7 +64,7 @@ template<typename samp_type> void recv_to_file(
     stream_cmd.stream_now = true;
     stream_cmd.time_spec = uhd::time_spec_t();
 
-    std::cout << "Commenting out issuing stream command" std::endl;
+    std::cout << "Commenting out issuing stream command" << std::endl;
     rx_stream->issue_stream_cmd(stream_cmd);
 
     typedef std::map<size_t,size_t> SizeMap;
