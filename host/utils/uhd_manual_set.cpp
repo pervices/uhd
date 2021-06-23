d8f5fe1fd08dc816461948d37060d5f6df1a0b74
Doug W <doug.w@pervices.com>
2021-06-23 21:20:15 +0000

    UTILS: add help option and implemented arguments for manual set property

--- host/utils/uhd_manual_set.cpp
+++ host/utils/uhd_manual_set.cpp
@@ -31,14 +31,34 @@ namespace
 
 int UHD_SAFE_MAIN(int argc, char *argv[])
 {
-    (void) argc;
-    (void) argv;
 
     uhd::set_thread_priority_safe();
 
+    //variables to be set by po
+    std::string path, value;
+
+    //setup the program options
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "help message")
+        ("path", po::path<std::string>(&path)->default_value(""), "The path for the value in the UHD state tree to set")
+        ("value", po::value<std::string>(&value)->default_value(""), "The value the variable it to be set to")
+
+    //print the help message
+    if (vm.count("help")) {
+        std::cout << boost::format("UHD set value at path %s") % desc << std::endl;
+        std::cout
+            << std::endl
+            << "This set the value at the path specified.\n"
+            << std::endl;
+        return ~0;
+    }
+
     uhd::usrp::multi_usrp::sptr usrp = uhd::usrp::multi_usrp::make(std::string(""));
 
-    set_at_path(usrp, );
+    set_at_path(usrp, path, value);
+
+    std::cout << "Done" << std::endl;
 
     return 0;
 }
