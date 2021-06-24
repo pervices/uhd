c171049da9c606430213753dd2e5f18304593880
Doug W <doug.w@pervices.com>
2021-06-24 18:26:23 +0000

    UTILS: added support for ints and doubles to manual get

--- host/utils/uhd_manual_get.cpp
+++ host/utils/uhd_manual_get.cpp
@@ -8,13 +8,33 @@ namespace po = boost::program_options;
 namespace
 {
 
-    void get_at_path(uhd::usrp::multi_usrp::sptr& usrp, std::string path)
+    void get_string_at_path(uhd::usrp::multi_usrp::sptr& usrp, std::string path)
     {
         std::cout << __FUNCTION__ << std::endl;
 
         std::cout << "Getting value at: " << path << std::endl;
         //gets the value from the state tree
         std::string value;
+        usrp->get_tree_value(path, value);
+
+    }
+    void get_int_at_path(uhd::usrp::multi_usrp::sptr& usrp, std::string path)
+    {
+        std::cout << __FUNCTION__ << std::endl;
+
+        std::cout << "Getting value at: " << path << std::endl;
+        //gets the value from the state tree
+        int value;
+        usrp->get_tree_value(path, value);
+        std::cout << "Value: " << value << std::endl;
+    }
+    void get_double_at_path(uhd::usrp::multi_usrp::sptr& usrp, std::string path)
+    {
+        std::cout << __FUNCTION__ << std::endl;
+
+        std::cout << "Getting value at: " << path << std::endl;
+        //gets the value from the state tree
+        double value;
         usrp->get_tree_value(path, value);
         std::cout << "Value: " << value << std::endl;
     }
@@ -27,13 +47,14 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
     uhd::set_thread_priority_safe();
 
     //variables to be set by po
-    std::string path;
+    std::string path, type;
 
     //setup the program options
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "help message")
         ("path", po::value<std::string>(&path)->default_value(""), "The path for the value in the UHD state tree to get")
+        ("type", po::value<std::string>(&type)->default_value("string"), "The data type of the variable to get")
     ;
 
     po::variables_map vm;
@@ -52,7 +73,9 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 
     uhd::usrp::multi_usrp::sptr usrp = uhd::usrp::multi_usrp::make(std::string(""));
 
-    get_at_path(usrp, path);
+    if(type.compare("double")==0) get_double_at_path(usrp, path);
+    else if (type.compare("int")==0) get_int_at_path(usrp, path);
+    else get_string_at_path(usrp, path);
 
     std::cout << "Done" << std::endl;
 
