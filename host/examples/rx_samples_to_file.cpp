5ecb8a0cae57738e54064a251019dbd9de68b35d
Doug W <doug.w@pervices.com>
2021-07-06 14:57:40 +0000

    EXAMPLES: testing if the example works without timeout check removed

--- host/examples/rx_samples_to_file.cpp
+++ host/examples/rx_samples_to_file.cpp
@@ -89,10 +89,11 @@ template<typename samp_type> void recv_to_file(
         size_t num_rx_samps =
             rx_stream->recv(&buff.front(), buff.size(), md, 3.0, enable_size_map);
 
-        if (md.error_code == uhd::rx_metadata_t::ERROR_CODE_TIMEOUT) {
-            std::cout << boost::format("Timeout while streaming") << std::endl;
-            break;
-        }
+         if (md.error_code == uhd::rx_metadata_t::ERROR_CODE_TIMEOUT) {
+             std::cout << boost::format("Timeout while streaming") << std::endl;
+             //break;
+             continue;
+         }
         if (md.error_code == uhd::rx_metadata_t::ERROR_CODE_OVERFLOW){
             if (overflow_message) {
                 overflow_message = false;
