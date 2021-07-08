04af83025b57e36a2842e1710fd3d61a6bf13268
Doug W <doug.w@pervices.com>
2021-07-08 22:40:58 +0000

    CYAN_4R4T: attempt to fix missing type error

--- host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.hpp
+++ host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.hpp
@@ -254,7 +254,8 @@ private:
     double get_tx_freq(size_t chan = 0);
 
     static void get_tx_endpoint( uhd::property_tree::sptr tree, const size_t & chan, std::string & ip_addr, uint16_t & udp_port, std::string & sfp );
-};￼
+};
+
 }
 }
 
