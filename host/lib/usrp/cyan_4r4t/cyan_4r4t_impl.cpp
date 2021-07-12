304763953acd0f28b782e8d34b05f79a2902aceb
Doug W <doug.w@pervices.com>
2021-07-12 20:53:31 +0000

    CYAN_4R4T: added high band
    I think that the mcu handles differences between high a low band
    If not, more changes will be needed

--- host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.cpp
+++ host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.cpp
@@ -1671,7 +1671,9 @@ static tune_result_t tune_xx_subdev_and_dsp( const double xx_sign, property_tree
 				// in low band, we only use the DSP to tune
 				target_rf_freq = 0;
 				break;
+            //DW 2021-07-12: we think that low and high band differences are managed by the mcu
 			case MID_BAND:
+            case HIGH_BAND:
 				dsp_nco_shift = choose_dsp_nco_shift( clipped_requested_freq, dsp_subtree );
 				// in mid band, we use the LO for most of the shift, and use the DSP for the difference
 				target_rf_freq = rf_range.clip( clipped_requested_freq - dsp_nco_shift );
