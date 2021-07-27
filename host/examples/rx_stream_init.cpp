f9f88394cd3f249309722e96d3b4c3cdf22f1bfd
Doug W <doug.w@pervices.com>
2021-07-27 19:12:09 +0000

    RX_STREAM_INIT: waits for pre exec to close

--- host/examples/rx_stream_init.cpp
+++ host/examples/rx_stream_init.cpp
@@ -99,6 +99,7 @@ template<typename samp_type> void recv_to_file(
     if(pre_pid != 0) {
         std::cout << "Stopping pre exec" <<std::endl;
         kill(pre_pid, SIGTERM);
+        waitpid(pre_pid);
     }
 
     stream_cmd.stream_mode = uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
