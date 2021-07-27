a2362d76049982cd262467f6104cd20960ceb419
Doug W <doug.w@pervices.com>
2021-07-27 16:25:57 +0000

    Fixing last commit

--- host/examples/rx_stream_init.cpp
+++ host/examples/rx_stream_init.cpp
@@ -76,8 +76,7 @@ template<typename samp_type> void recv_to_file(
         char *args;
         extract_args(args, pre_exec_file);
         const char *const_args = args;
-        const char *file = *const_args[0];
-        pre_pid = execvp(*file, *const_args);
+        pre_pid = execvp(args[0].c_str(), *const_args);
     }
 
     rx_stream->issue_stream_cmd(stream_cmd);
