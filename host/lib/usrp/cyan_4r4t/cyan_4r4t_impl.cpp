31d4f8c76cc832fefbda62ad0f848a2a74b0cd68
Doug W <doug.w@pervices.com>
2021-08-04 16:14:39 +0000

    CYAN_4R4T: removed commented out code that is no longer useful

--- host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.cpp
+++ host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.cpp
@@ -1494,21 +1494,6 @@ int cyan_4r4t_impl::get_rx_xg_intf(int channel) {
 
 void cyan_4r4t_impl::get_tx_endpoint( uhd::property_tree::sptr tree, const size_t & chan, std::string & ip_addr, uint16_t & udp_port, std::string & sfp ) {
 
-// 	switch( chan ) {
-// 	case 0:
-// 		sfp = "sfpa";
-// 		break;
-// 	case 1:
-//         sfp = "sfpb";
-//         break;
-//     case 2:
-//         sfp = "sfpc";
-//         break;
-// 	case 3:
-// 		sfp = "sfpd";
-// 		break;
-// 	}
-
 	const std::string chan_str( 1, 'A' + chan );
 	const fs_path mb_path   = "/mboards/0";
 	const fs_path prop_path = mb_path / "tx_link";
