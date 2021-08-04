2437600548c0dd15eaa04b9807adbce041f3e24a
Doug W <doug.w@pervices.com>
2021-08-04 17:57:29 +0000

    Fixing list commit

--- host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.cpp
+++ host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.cpp
@@ -1857,7 +1857,7 @@ void cyan_4r4t_impl::set_tx_gain(double gain, const std::string &name, size_t ch
 
 
         //const fs_path tx_fe_path = mb_path / "dboards" / num / "tx_frontends" / chan;
-        _tree->access<double>(tx_fe_path(chan)/ "gain" / "value").set(gain);
+        _tree->access<double>(multi_usrp::tx_rf_fe_root(chan)/ "gain" / "value").set(gain);
         return;
         }
     for (size_t c = 0; c < CYAN_4R4T_TX_CHANNELS; c++){
