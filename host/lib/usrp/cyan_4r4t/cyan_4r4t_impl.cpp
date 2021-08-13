6891b11511289409b683f785dbe724b5f24a95fb
Doug W <doug.w@pervices.com>
2021-08-13 17:58:48 +0000

    IP Binding: allows the program to continue if IP binding fails (rx)

--- host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.cpp
+++ host/lib/usrp/cyan_4r4t/cyan_4r4t_impl.cpp
@@ -1217,17 +1217,23 @@ cyan_4r4t_impl::cyan_4r4t_impl(const device_addr_t &_device_addr)
 		zcxp.num_send_frames = 0;
 		zcxp.num_recv_frames = DEFAULT_NUM_FRAMES;
 
-		_mbc[mb].rx_dsp_xports.push_back(
-			udp_stream_zero_copy::make(
-				_tree->access<std::string>( rx_link_path / "ip_dest" ).get(),
-				std::stoi( _tree->access<std::string>( rx_link_path / "port" ).get() ),
-				"127.0.0.1",
-				1,
-				zcxp,
-				bp,
-				device_addr
-			)
-		);
+
+        try {
+
+            _mbc[mb].rx_dsp_xports.push_back(
+                udp_stream_zero_copy::make(
+                    _tree->access<std::string>( rx_link_path / "ip_dest" ).get(),
+                    std::stoi( _tree->access<std::string>( rx_link_path / "port" ).get() ),
+                    "127.0.0.1",
+                    1,
+                    zcxp,
+                    bp,
+                    device_addr
+                )
+            );
+        } catch (...) {
+            UHD_LOGGER_ERROR("CYAN_4R4T") << "Unable to bind ip adress, certain features may not work. \n IP: " << _tree->access<std::string>( rx_link_path / "ip_dest" ).get() << std::endl;
+        }
     }
 
     // loop for all TX chains
