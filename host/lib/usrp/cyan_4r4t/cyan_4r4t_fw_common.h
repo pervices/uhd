202c140c52d0da753811bec596f3d7cd22d6c6b5
Doug W <doug.w@pervices.com>
2022-01-27 18:30:46 +0000

    CYAN_4R4T: reducing target buffer size to prevent oflows

--- host/lib/usrp/cyan_4r4t/cyan_4r4t_fw_common.h
+++ host/lib/usrp/cyan_4r4t/cyan_4r4t_fw_common.h
@@ -81,7 +81,7 @@
 // Cyan 4r4t Buffer Size
 #define CYAN_4R4T_BUFF_SIZE	(2048*140*512/32)
 //how full the system shoudl try to keep the buffer
-#define CYAN_4R4T_BUFF_PERCENT 0.7
+#define CYAN_4R4T_BUFF_PERCENT 0.5
 
 // Cyan 4r4t RF Settings
 #define CYAN_4R4T_RF_TX_GAIN_RANGE_START	0.0
