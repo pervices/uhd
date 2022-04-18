8b3334a183057bac1d5a81f633ba2560b249a012
Doug W <doug.w@pervices.com>
2022-04-18 16:36:11 +0000

    CYAN_8R: renaming io_impl to cyan_8r_io_impl to avoid confusion

--- host/lib/usrp/cyan_8r/cyan_8r_io_impl.cpp
+++ host/lib/usrp/cyan_8r/cyan_8r_io_impl.cpp
@@ -578,7 +578,7 @@ private:
 				std::printf("%10ld\t", uflow);
 #endif
 				if ( (uint64_t)-1 != ep.uflow && uflow != ep.uflow ) {
-					// XXX: @CF: 20170905: Eventually we want to return tx channel metadata as VRT49 context packets rather than custom packets. See usrp2/io_impl.cpp
+					// XXX: @CF: 20170905: Eventually we want to return tx channel metadata as VRT49 context packets rather than custom packets. See usrp2/cyan_8r_io_impl.cpp
 					// async_metadata_t metadata;
 					// load_metadata_from_buff( uhd::ntohx<boost::uint32_t>, metadata, if_packet_info, vrt_hdr, tick_rate, index );
 					metadata.channel = i;
@@ -594,7 +594,7 @@ private:
 				std::printf("%10ld", oflow);
 #endif
 				if ( (uint64_t)-1 != ep.oflow && oflow != ep.oflow ) {
-					// XXX: @CF: 20170905: Eventually we want to return tx channel metadata as VRT49 context packets rather than custom packets. See usrp2/io_impl.cpp
+					// XXX: @CF: 20170905: Eventually we want to return tx channel metadata as VRT49 context packets rather than custom packets. See usrp2/cyan_8r_io_impl.cpp
 					// async_metadata_t metadata;
 					// load_metadata_from_buff( uhd::ntohx<boost::uint32_t>, metadata, if_packet_info, vrt_hdr, tick_rate, index );
 					metadata.channel = i;
@@ -650,21 +650,21 @@ static const size_t vrt_send_header_offset_words32 = 0;
  * io impl details (internal to this file)
  * - alignment buffer
  **********************************************************************/
-struct cyan_8r_impl::io_impl{
+struct cyan_8r_impl::cyan_8r_io_impl{
 
-    io_impl(void):
+    cyan_8r_io_impl(void):
         async_msg_fifo(1000/*messages deep*/)
     {
         /* NOP */
     }
 
-    ~io_impl(void){
+    ~cyan_8r_io_impl(void){
     }
 
     //methods and variables for the viking scourge
     bounded_buffer<async_metadata_t> async_msg_fifo;
 
-    // TODO: @CF: 20180301: move time diff code into io_impl
+    // TODO: @CF: 20180301: move time diff code into cyan_8r_io_impl
 };
 
 /***********************************************************************
@@ -672,8 +672,8 @@ struct cyan_8r_impl::io_impl{
  **********************************************************************/
 void cyan_8r_impl::io_init(void){
 
-	// TODO: @CF: 20180301: move time diff code into io_impl
-	_io_impl = UHD_PIMPL_MAKE(io_impl, ());
+	// TODO: @CF: 20180301: move time diff code into cyan_8r_io_impl
+	_cyan_8r_io_impl = UHD_PIMPL_MAKE(cyan_8r_io_impl, ());
 
     //allocate streamer weak ptrs containers
     for (const std::string &mb : _mbc.keys()) {
@@ -791,7 +791,7 @@ bool cyan_8r_impl::recv_async_msg(
     async_metadata_t &async_metadata, double timeout
 ){
     boost::this_thread::disable_interruption di; //disable because the wait can throw
-    return _io_impl->async_msg_fifo.pop_with_timed_wait(async_metadata, timeout);
+    return _cyan_8r_io_impl->async_msg_fifo.pop_with_timed_wait(async_metadata, timeout);
 }
 
 /***********************************************************************
@@ -1141,9 +1141,9 @@ tx_streamer::sptr cyan_8r_impl::get_tx_stream(const uhd::stream_args_t &args_){
                     &get_fifo_lvl_udp, chan, _mbc[mb].fifo_ctrl_xports[dsp], _1, _2, _3, _4
                 ));
 
-                my_streamer->set_async_receiver(boost::bind(&bounded_buffer<async_metadata_t>::pop_with_timed_wait, &(_io_impl->async_msg_fifo), _1, _2));
+                my_streamer->set_async_receiver(boost::bind(&bounded_buffer<async_metadata_t>::pop_with_timed_wait, &(_cyan_8r_io_impl->async_msg_fifo), _1, _2));
 
-                my_streamer->set_async_pusher(boost::bind(&bounded_buffer<async_metadata_t>::push_with_pop_on_full, &(_io_impl->async_msg_fifo), _1));
+                my_streamer->set_async_pusher(boost::bind(&bounded_buffer<async_metadata_t>::push_with_pop_on_full, &(_cyan_8r_io_impl->async_msg_fifo), _1));
 
                 _mbc[mb].tx_streamers[chan] = my_streamer; //store weak pointer
                 break;
