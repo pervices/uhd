c1317f80da82ff17fa9ccad8995a1488a2ba36eb
Doug W <doug.w@pervices.com>
2022-05-02 20:53:11 +0000

    copying in modifications to example and only example

--- host/examples/rx_to_tx_loopback.cpp
+++ host/examples/rx_to_tx_loopback.cpp
@@ -30,6 +30,7 @@
 #include <math.h>
 #include <thread>
 #include <mutex>
+#include <shared_mutex>
 #include <unistd.h>
 #include <algorithm>
 #include <semaphore.h>
@@ -42,14 +43,18 @@ void sig_int_handler(int)
     stop_signal_called = true;
 }
 
-std::mutex mtx;
+// Ideally there should be a lock to prevent caching resulting in data not being shared, however I cannot find a way to specify only what needs to be locked
+// Due to the amount of data being shared any chaches will probably get emptied
+//std::shared_timed_mutex mtx;
+//number of buffers to use, must be a power of 2
 const size_t num_buffers = 8;
+const size_t valid_index_mask = num_buffers - 1;
 //how much percent of required samples between rx and tx buffer are used for each loopback
 const double host_buff_size = 0.25;
 // space at the end of buffer that is used to make it easier to avoid issues when samples can only be sent in specific amounts
 const size_t spare_buffer_space = 10000;
 //TODO change send function so that it will partially send instead of sending the entire buffer when the device's buffer would dip below the setpoint
-const size_t max_samples_per_tx = 10000;
+const size_t max_samples_per_tx = 8000;
 // The inner buffer is a buffer containing data received from each channel
 // The middle layer groups the buffer for each ch together
 // The outer layer exists so that the rx can receive to once set of buffers and tx the other, so that they don't need to lock and unlock every send/recv
@@ -65,43 +70,43 @@ void rx_run(uhd::rx_streamer::sptr rx_stream, double start_time, size_t total_nu
     size_t max_samples_per_buffer = buffers[0][0].size();
     uhd::rx_metadata_t rx_md;
 
+    std::vector<std::vector<std::complex<short>>> *active_buffer;
+    std::vector<std::complex<short>*> active_buff_ptrs(num_channels);
+
+    size_t samples_this_buffer = 0;
+
+    active_buffer = &buffers[active_buffer_index];
+    //std::unique_lock<std::shared_timed_mutex> lock(mtx);
+
     while ((num_acc_samps < total_num_samps || total_num_samps == 0) && !stop_signal_called) {
 
-        std::vector<std::vector<std::complex<short>>> *active_buffer;
-        active_buffer = &buffers[active_buffer_index];
-
-        //std::unique_lock<std::mutex> lock(mtx);
-
-        size_t samples_this_buffer = 0;
-
-        while (samples_this_buffer + spare_buffer_space < max_samples_per_buffer && !stop_signal_called) {
-
-            std::vector<std::complex<short>*> active_buff_ptrs(num_channels);
-
-            for(size_t n = 0; n < num_channels; n++) {
-                active_buff_ptrs[n] = &(*active_buffer)[n][samples_this_buffer];
-            }
-
-            // receive a single packet
-            size_t samps_received = rx_stream->recv(active_buff_ptrs, max_samples_per_buffer - samples_this_buffer, rx_md, timeout, true);
-
-            //use small timeout for subsequent packets
-            timeout = 0.1;
-            num_acc_samps+=samps_received;
-            samples_this_buffer+=samps_received;
+        for(size_t n = 0; n < num_channels; n++) {
+            active_buff_ptrs[n] = &(*active_buffer)[n][samples_this_buffer];
         }
 
+        // receive a single packet
+        size_t samps_received = rx_stream->recv(active_buff_ptrs, max_samples_per_buffer - samples_this_buffer, rx_md, timeout, true);
 
-        buffer_used[active_buffer_index] = samples_this_buffer;
-        //lock.unlock();
+        num_acc_samps+=samps_received;
+        samples_this_buffer+=samps_received;
 
-        sem_post(&buff_ready[active_buffer_index]);
+        if(samples_this_buffer + spare_buffer_space > max_samples_per_buffer) {
+            //lock.unlock();
+            buffer_used[active_buffer_index] = samples_this_buffer;
+            sem_post(&buff_ready[active_buffer_index]);
+            active_buffer_index++;
+            //caps the active to be less than the number of buffers
+            active_buffer_index = active_buffer_index & valid_index_mask;
 
-        active_buffer_index++;
-        if(active_buffer_index >= num_buffers) {
-            active_buffer_index = 0;
-         }
+            active_buffer = &buffers[active_buffer_index];
+
+            samples_this_buffer = 0;
+            //lock.lock();
+        }
     }
+
+    //lock.unlock();
+
     uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
     rx_stream->issue_stream_cmd(stream_cmd);
 }
@@ -109,7 +114,7 @@ void tx_run( uhd::tx_streamer::sptr tx_stream, double start_time, size_t total_n
     size_t num_acc_samps = 0;
     size_t active_buffer_index = 0;
     size_t num_channels = buffers[0].size();
-    size_t max_samples_per_buffer = buffers[0][0].size();
+    size_t max_samples_per_buffer = std::min(buffers[0][0].size(), max_samples_per_tx);
     // Set up Tx metadata. We start streaming a bit in the future
     uhd::tx_metadata_t tx_md;
     tx_md.start_of_burst = true;
@@ -117,48 +122,39 @@ void tx_run( uhd::tx_streamer::sptr tx_stream, double start_time, size_t total_n
     tx_md.has_time_spec  = true;
     tx_md.time_spec = uhd::time_spec_t(start_time);
 
-    tx_stream->send("", 0, tx_md);
+    std::vector<std::complex<short>*> active_buff_ptrs(num_channels);
 
-    tx_md.start_of_burst = false;
-    tx_md.has_time_spec = false;
+    std::vector<std::vector<std::complex<short>>> *active_buffer;
+    active_buffer = &buffers[active_buffer_index];
+    size_t samples_this_buffer = 0;
+
+    sem_wait(&buff_ready[active_buffer_index]);
+    size_t samples_to_send_this_buffer = buffer_used[active_buffer_index];
+    //std::shared_lock<std::shared_timed_mutex> lock(mtx);
     
     while ((num_acc_samps < total_num_samps || total_num_samps == 0) && !stop_signal_called) {
 
-        sem_wait(&buff_ready[active_buffer_index]);
-
-        std::vector<std::vector<std::complex<short>>> *active_buffer;
-        active_buffer = &buffers[active_buffer_index];
-        size_t samples_to_send_this_buffer = buffer_used[active_buffer_index];
-
-        std::unique_lock<std::mutex> lock(mtx);
-
-        size_t samples_this_buffer = 0;
-
-        while (samples_this_buffer + spare_buffer_space < max_samples_per_buffer && !stop_signal_called) {
-
-            std::vector<std::complex<short>*> active_buff_ptrs(num_channels);
-
-            for(size_t n = 0; n < num_channels; n++) {
-                active_buff_ptrs[n] = &(*active_buffer)[n][samples_this_buffer];
-            }
-
-            size_t samples_sent = tx_stream->send(active_buff_ptrs, std::min(samples_to_send_this_buffer - samples_this_buffer, max_samples_per_tx), tx_md);
-
-            tx_md.start_of_burst = false;
-            tx_md.has_time_spec = false;
-
-
-            samples_this_buffer+=samples_sent;
-            num_acc_samps+=samples_sent;
+        if(samples_this_buffer >= samples_to_send_this_buffer) {
+            //lock.unlock();
+            active_buffer_index++;
+            active_buffer_index = active_buffer_index & valid_index_mask;
+            samples_this_buffer = 0;
+            samples_to_send_this_buffer = buffer_used[active_buffer_index];
+            active_buffer = &buffers[active_buffer_index];
+            sem_wait(&buff_ready[active_buffer_index]);
+            //lock.lock();
         }
 
-        lock.unlock();
-
-        active_buffer_index++;
-        if(active_buffer_index >= num_buffers) {
-            active_buffer_index = 0;
+        for(size_t n = 0; n < num_channels; n++) {
+            active_buff_ptrs[n] = &(*active_buffer)[n][samples_this_buffer];
         }
-        
+
+        size_t samples_sent = tx_stream->send(active_buff_ptrs, std::min(samples_to_send_this_buffer - samples_this_buffer, max_samples_per_buffer), tx_md);
+        samples_this_buffer+=samples_sent;
+        num_acc_samps+=samples_sent;
+
+        tx_md.start_of_burst = false;
+        tx_md.has_time_spec = false;
     }
     tx_md.end_of_burst = true;
     tx_stream->send("", 0, tx_md);
