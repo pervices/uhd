16b7df946267ca0eb7c2d9f6c14c3cc0489b86a2
Doug W <doug.w@pervices.com>
2022-05-24 15:03:49 +0000

    Revert "Reverting to old version number, required for compatibility with GNU Radio"
    This reverts commit 99347e16f4044a71d356afd2b20192f61a4cf973.

--- host/cmake/Modules/UHDVersion.cmake
+++ host/cmake/Modules/UHDVersion.cmake
@@ -19,11 +19,11 @@ find_package(Git QUIET)
 #     release)
 #  - set UHD_VERSION_DEVEL to true for master and development branches
 ########################################################################
-set(UHD_VERSION_MAJOR   3)
-set(UHD_VERSION_API    13)
-set(UHD_VERSION_ABI     0)
-set(UHD_VERSION_PATCH   1)
-set(UHD_VERSION_DEVEL FALSE)
+set(UHD_VERSION_MAJOR 4)
+set(UHD_VERSION_API   2)
+set(UHD_VERSION_ABI   0)
+set(UHD_VERSION_PATCH git)
+set(UHD_VERSION_DEVEL TRUE)
 
 ########################################################################
 # If we're on a development branch, we skip the patch version
