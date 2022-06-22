cae6c2584db8b6064254fb413a5c82496482a6de
Doug W <doug.w@pervices.com>
2022-06-22 15:10:24 +0000

    CYAN_4R4T_3G: adding warning if JESD link is down

--- host/lib/usrp/cyan_4r4t_3g/cyan_4r4t_3g_io_impl.cpp
+++ host/lib/usrp/cyan_4r4t_3g/cyan_4r4t_3g_io_impl.cpp
@@ -1000,6 +1000,27 @@ rx_streamer::sptr cyan_4r4t_3g_impl::get_rx_stream(const uhd::stream_args_t &arg
     //sets all tick and samp rates on this streamer
     this->update_rates();
 
+    for (size_t chan_i = 0; chan_i < args.channels.size(); chan_i++){
+        const size_t chan = args.channels[chan_i];
+        size_t num_chan_so_far = 0;
+        for (const std::string &mb : _mbc.keys()) {
+            num_chan_so_far += _mbc[mb].rx_chan_occ;
+            if (chan < num_chan_so_far){
+
+                // XXX: @CF: this is so nasty..
+                const std::string ch    = "Channel_" + std::string( 1, 'A' + chan );
+                const fs_path mb_path   = "/mboards/" + mb;
+                const fs_path rx_path   = mb_path / "rx";
+
+                _tree->access<std::string>(rx_path / chan / "jesd/status").set("1");
+                std::string jesd_status = _tree->access<std::string>(rx_path / chan / "jesd/status").get();
+                if(jesd_status.compare(0, 4, "good")) {
+                    UHD_LOGGER_WARNING(CYAN_4R4T_3G_DEBUG_NAME_C) << ch << ": unable to establish JESD link. This streamer will not work." << std::endl;
+                }
+            }
+        }
+    }
+
     // XXX: @CF: 20180117: Give any transient errors in the time-convergence PID loop sufficient time to subsidte. KB 4312
 	for( ;! time_diff_converged(); ) {
 		usleep( 10000 );
