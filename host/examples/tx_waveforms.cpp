ba85f8ab7acef9c068878edfcb696d2daaec805c
Doug W <doug.w@pervices.com>
2022-06-23 15:55:29 +0000

    test fix

--- host/examples/tx_waveforms.cpp
+++ host/examples/tx_waveforms.cpp
@@ -223,7 +223,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
         spb = tx_stream->get_max_num_samps()*10;
     }
 
-    std::variant<std::vector<std::complex<float>>, std::vector<std::complex<int16_t>>> buff;
+    boost::variant<std::vector<std::complex<float>>, std::vector<std::complex<int16_t>>> buff;
 
     if(cpu_format == "sc16") {
         buff = std::vector<std::complex<int16_t>>(buff(spb));
