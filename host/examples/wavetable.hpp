667587f412ae0d793b8c528f84aa758ef23d57ec
Doug W <doug.w@pervices.com>
2022-06-23 20:10:08 +0000

    debug msg

--- host/examples/wavetable.hpp
+++ host/examples/wavetable.hpp
@@ -87,6 +87,7 @@ public:
 
     inline std::complex<cpu_format_type> operator()(const size_t index) const
     {
+        std::cout "_wave_table[index % wave_table_len]: " << _wave_table[index % wave_table_len] << std::endl;
         return _wave_table[index % wave_table_len];
     }
 
