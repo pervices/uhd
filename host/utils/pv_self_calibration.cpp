de33943ed9c2d7a8bb3a2d39dabd845abebccc1d
Doug W <doug.w@pervices.com>
2022-12-14 17:58:13 +0000

    continuing

--- host/utils/pv_self_calibration.cpp
+++ host/utils/pv_self_calibration.cpp
@@ -15,8 +15,11 @@
 #include <complex>
 #include <iostream>
 #include <thread>
+#include <algorithm>
 
 namespace po = boost::program_options;
+
+static const size_t self_calibration_nsamps = 2000;
 
 int UHD_SAFE_MAIN(int argc, char* argv[])
 {
@@ -61,10 +64,22 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
         }
     }
 
-    std::cout << "Beging self calibration" << std::endl;
+    std::cout << "Begining self calibration" << std::endl;
+
+    size_t num_rx_channels = usrp->get_rx_num_channels();
+
+    for(size_t ch = 0; ch < num_rx_channels; ch++){
+        //set the Rx rf gain
+        usrp->set_rx_gain(0, ch);
+    }
+
+        // setting tuning Tx frequency
+    for(size_t ch = 0; ch < num_rx_channels; ch++){
+        uhd::tune_request_t rx_tune_request(0);
+        usrp->set_rx_freq(rx_tune_request, ch);
+    }
 
     // set the rx sample rate to maximum
-    // The server will clip this value down to the maximum
     usrp->set_rx_rate(usrp->get_max_rate_mhz());
 
     std::cout << "Setting device timestamp to 0" << std::endl;
@@ -72,72 +87,60 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
 
     // detect which channels to use
     std::vector<size_t> channel_nums;
-    size_t num_rx_channels = usrp->get_rx_num_channels();
     for (size_t ch = 0; ch < num_rx_channels; ch++) {
             channel_nums.push_back(ch);
     }
 
-//     // create a receive streamer
-//     // linearly map channels (index0 = channel0, index1 = channel1, ...)
-//     uhd::stream_args_t stream_args("sc16"); // complex floats
-//     stream_args.channels             = channel_nums;
-//     uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(stream_args);
-//
-//     // setup streaming
-//     std::cout << std::endl;
-//     std::cout << boost::format("Begin streaming %u samples, %f seconds in the future...")
-//                      % total_num_samps % seconds_in_future
-//               << std::endl;
-//     uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_NUM_SAMPS_AND_DONE);
-//     stream_cmd.num_samps  = total_num_samps;
-//     stream_cmd.stream_now = false;
-//     stream_cmd.time_spec  = uhd::time_spec_t(seconds_in_future);
-//     rx_stream->issue_stream_cmd(stream_cmd); // tells all channels to stream
-//
-//     // meta-data will be filled in by recv()
-//     uhd::rx_metadata_t md;
-//
-//     // allocate buffers to receive with samples (one buffer per channel)
-//     const size_t samps_per_buff = rx_stream->get_max_num_samps();
-//     std::vector<std::vector<std::complex<float>>> buffs(
-//         usrp->get_rx_num_channels(), std::vector<std::complex<float>>(samps_per_buff));
-//
-//     // create a vector of pointers to point to each of the channel buffers
-//     std::vector<std::complex<float>*> buff_ptrs;
-//     for (size_t i = 0; i < buffs.size(); i++)
-//         buff_ptrs.push_back(&buffs[i].front());
-//
-//     // the first call to recv() will block this many seconds before receiving
-//     double timeout = seconds_in_future + 0.1; // timeout (delay before receive + padding)
-//
-//     size_t num_acc_samps = 0; // number of accumulated samples
-//     while (num_acc_samps < total_num_samps) {
-//         // receive a single packet
-//         size_t num_rx_samps = rx_stream->recv(buff_ptrs, samps_per_buff, md, timeout);
-//
-//         // use a small timeout for subsequent packets
-//         timeout = 0.1;
-//
-//         // handle the error code
-//         if (md.error_code == uhd::rx_metadata_t::ERROR_CODE_TIMEOUT)
-//             break;
-//         if (md.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE) {
-//             throw std::runtime_error(
-//                 str(boost::format("Receiver error %s") % md.strerror()));
-//         }
-//
-//         if (verbose)
-//             std::cout << boost::format(
-//                              "Received packet: %u samples, %u full secs, %f frac secs")
-//                              % num_rx_samps % md.time_spec.get_full_secs()
-//                              % md.time_spec.get_frac_secs()
-//                       << std::endl;
-//
-//         num_acc_samps += num_rx_samps;
-//     }
-//
-//     if (num_acc_samps < total_num_samps)
-//         std::cerr << "Receive timeout before all samples received..." << std::endl;
+    // create a receive streamer
+    // linearly map channels (index0 = channel0, index1 = channel1, ...)
+    uhd::stream_args_t stream_args("sc16"); // complex floats
+    stream_args.channels = channel_nums;
+    uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(stream_args);
+
+    // setup streaming
+    uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_NUM_SAMPS_AND_DONE);
+    stream_cmd.num_samps  = self_calibration_nsamps;
+    stream_cmd.stream_now = true;
+    stream_cmd.time_spec  = uhd::time_spec_t(0.0);
+    rx_stream->issue_stream_cmd(stream_cmd); // tells all channels to stream
+
+    // meta-data will be filled in by recv()
+    uhd::rx_metadata_t md;
+
+    // allocate buffers to receive with samples (one buffer per channel)
+    const size_t samps_per_buff = std::min(rx_stream->get_max_num_samps(), self_calibration_nsamps);
+    std::vector<std::vector<std::complex<float>>> buffs(
+        usrp->get_rx_num_channels(), std::vector<std::complex<float>>(samps_per_buff));
+
+
+    size_t num_acc_samps = 0; // number of accumulated samples
+    double timeout = 5;
+    while (num_acc_samps < self_calibration_nsamps) {
+
+        // create a vector of pointers to point to each of the channel buffers
+        std::vector<std::complex<float>*> buff_ptrs;
+        for (size_t i = 0; i < buffs.size(); i++) {
+            buff_ptrs.push_back(&buffs[i].at(num_acc_samps));
+        }
+        // receive a single packet
+        size_t num_rx_samps = rx_stream->recv(buff_ptrs, std::min(samps_per_buff, self_calibration_nsamps - num_acc_samps), md, timeout);
+
+        // use a small timeout for subsequent packets
+        timeout = 0.1;
+
+        // handle the error code
+        if (md.error_code == uhd::rx_metadata_t::ERROR_CODE_TIMEOUT)
+            break;
+        if (md.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE) {
+            throw std::runtime_error(
+                str(boost::format("Receiver error %s") % md.strerror()));
+        }
+
+        num_acc_samps += num_rx_samps;
+    }
+
+    if (num_acc_samps < self_calibration_nsamps)
+        std::cerr << "Receive timeout before all samples received..." << std::endl;
 
     // finished
     std::cout << std::endl << "Done!" << std::endl << std::endl;
