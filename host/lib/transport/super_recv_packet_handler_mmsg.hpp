342404d46d26f2af49bb27532ad49b4c1359a429
Doug W <doug.w@pervices.com>
2023-06-26 20:30:08 +0000

    RECV: adding end of burst

--- host/lib/transport/super_recv_packet_handler_mmsg.hpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.hpp
@@ -194,19 +194,30 @@ public:
         }
 
         // Returns the number of samples requested, if there were enough samples in the cache
-        if(!bytes_to_recv) {
+        if(!bytes_to_recv || cached_end_of_burst) {
             metadata.error_code = rx_metadata_t::ERROR_CODE_NONE;
+
+            // Sets the end of burst flag if cached, the clears it
+            metadata.end_of_burst = cached_end_of_burst;
+            cached_end_of_burst = false;
+
+            //TODO: simulate timestamp for cached samples
             return nsamps_per_buff;
         }
 
         // Receives packets, data is stores in recv_buffer, metadata is stored in ch_recv_buffer_info.headers
         metadata.error_code = recv_multiple_packets(*recv_buffer, cached_bytes_to_copy, cached_bytes_to_copy + bytes_to_recv, timeout);
 
+        // Setting metadata fields
+
         metadata.has_time_spec = true;
         metadata.time_spec = time_spec_t::from_ticks(ch_recv_buffer_info_group[0].vrt_metadata[0].tsf, _sample_rate);
 
         // Check for overflow errors and flushed packets to keep buffers aligned
         size_t aligned_bytes = align_buffs(metadata.error_code) + cached_bytes_to_copy;
+
+
+        metadata.end_of_burst = detect_end_of_burst();
 
         size_t final_nsamps = aligned_bytes/_BYTES_PER_SAMPLE;
 
@@ -247,6 +258,8 @@ private:
     size_t previous_sample_cache_used = 0;
     // Maximum sequence number
     const size_t sequence_number_mask = 0xf;
+    // Stores if end of burst
+    bool cached_end_of_burst = false;
     // Stores information about packets received for each channel
     // Note: this is not meant to be persistent between reads, it is done this way to avoid deallocating and reallocating memory
     struct ch_recv_buffer_info {
@@ -688,6 +701,41 @@ private:
             const ref_vector<void*> user_buffer_ch(user_buffer_ptrs[n]);
             // Converts the samples
             _converter->conv(_intermediate_recv_buffer_pointers[n], user_buffer_ch, num_samples);
+        }
+    }
+
+    bool detect_end_of_burst() {
+        bool end_of_burst_received = false;
+        for(auto& ch_recv_buffer_info_i : ch_recv_buffer_info_group) {
+            if(ch_recv_buffer_info_i.num_headers_used > 0) {
+                // Set end of burst if received on any channel
+                if(ch_recv_buffer_info_i.vrt_metadata[ch_recv_buffer_info_i.num_headers_used].eob) {
+                    end_of_burst_received = true;
+                }
+            }
+        }
+
+        bool should_cache_eob = end_of_burst_received;
+        if(end_of_burst_received) {
+            for(auto& ch_recv_buffer_info_i : ch_recv_buffer_info_group) {
+                if(ch_recv_buffer_info_i.sample_cache_used == 0) {
+                    should_cache_eob = false;
+                }
+            }
+        }
+
+        // EOB received, but there as samples in the cache so it should be saved until the next receive
+        if(should_cache_eob && end_of_burst_received) {
+            cached_end_of_burst = true;
+            return false;
+        // EOB received and there are no samples in the cache so clear EOB cache and set metadata flag
+        } else if(end_of_burst_received) {
+            cached_end_of_burst = false;
+            return true;
+        } else {
+        // Clear EOB cache, should be unreachable because
+            cached_end_of_burst = false;
+            return false;
         }
     }
 
