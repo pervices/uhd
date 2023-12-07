c73e9a35f0f1fb3623ba7fd294de3231921f020d
Chen Jin <chenjin9899@gmail.com>
2023-12-07 16:12:58 -0500

    only turn off rx time_disable

--- host/examples/txrx_trigger_loopback.cpp
+++ host/examples/txrx_trigger_loopback.cpp
@@ -114,10 +114,10 @@ void rx_run(uhd::rx_streamer::sptr rx_stream, double start_time, uint64_t num_tr
                 const std::string value = "0";
                 usrp->set_tree_value(path, value);
 
-                // const std::string root { "/mboards/0/rx/" + std::to_string(channel_nums[n]) + "/" };
-                // const std::string time_disable_path {root + "trigger/time_disable" };
-                // const std::string time_disable_value = "1";
-                // usrp->set_tree_value(time_disable_path, time_disable_value);
+                const std::string root { "/mboards/0/rx/" + std::to_string(channel_nums[n]) + "/" };
+                const std::string time_disable_path {root + "trigger/time_disable" };
+                const std::string time_disable_value = "1";
+                usrp->set_tree_value(time_disable_path, time_disable_value);
 
                 // const std::string tx_root { "/mboards/0/tx/" + std::to_string(channel_nums[n]) + "/" };
                 // const std::string tx_time_disable_path {root + "trigger/time_disable" };
