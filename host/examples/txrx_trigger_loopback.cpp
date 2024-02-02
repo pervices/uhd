e42536894d0225b4683ab9c60c8d38a82ca0d654
Chen Jin <chenjin9899@gmail.com>
2024-02-02 11:42:47 -0500

    disabled vita

--- host/examples/txrx_trigger_loopback.cpp
+++ host/examples/txrx_trigger_loopback.cpp
@@ -170,21 +170,21 @@ void rx_run(uhd::rx_streamer::sptr rx_stream, double start_time, uint64_t num_tr
 
     //re-enables waiting for vita commands (regular stream commands)
     // Disables the temporary workaround for the lack of STREAM_MODE_NUM_SAMPS_AND_MORE implementation
-    for(size_t n = 0; n < channel_nums.size(); n++) {
-        const std::string path { "/mboards/0/rx_link/" + std::to_string(channel_nums[n]) + "/vita_en" };
-        const std::string value = "1";
-        usrp->set_tree_value(path, value);
+    // for(size_t n = 0; n < channel_nums.size(); n++) {
+    //     const std::string path { "/mboards/0/rx_link/" + std::to_string(channel_nums[n]) + "/vita_en" };
+    //     const std::string value = "1";
+    //     usrp->set_tree_value(path, value);
 
-        // const std::string root { "/mboards/0/rx/" + std::to_string(channel_nums[n]) + "/" };
-        // const std::string time_disable_path {root + "trigger/time_disable" };
-        // const std::string time_disable_value = "0";
-        // usrp->set_tree_value(time_disable_path, time_disable_value);
+    //     // const std::string root { "/mboards/0/rx/" + std::to_string(channel_nums[n]) + "/" };
+    //     // const std::string time_disable_path {root + "trigger/time_disable" };
+    //     // const std::string time_disable_value = "0";
+    //     // usrp->set_tree_value(time_disable_path, time_disable_value);
 
-        // const std::string tx_root { "/mboards/0/tx/" + std::to_string(channel_nums[n]) + "/" };
-        // const std::string tx_time_disable_path {root + "trigger/time_disable" };
-        // const std::string tx_time_disable_value = "0";
-        // usrp->set_tree_value(tx_time_disable_path, tx_time_disable_value);
-    }
+    //     // const std::string tx_root { "/mboards/0/tx/" + std::to_string(channel_nums[n]) + "/" };
+    //     // const std::string tx_time_disable_path {root + "trigger/time_disable" };
+    //     // const std::string tx_time_disable_value = "0";
+    //     // usrp->set_tree_value(tx_time_disable_path, tx_time_disable_value);
+    // }
 
 }
 
