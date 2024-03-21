98c0cc92151539a158141d9cc9fc1bee669381da
Doug W <doug.w@pervices.com>
2024-03-21 14:19:35 +0000

    debug msg

--- host/lib/transport/super_recv_packet_handler_mmsg.hpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.hpp
@@ -541,8 +541,10 @@ private:
                 int num_packets_received_this_recv;
                 if(recv_return >= 0) {
                     num_packets_received_this_recv = 1;
+                    printf("Packet received\n");
                 } else {
                     num_packets_received_this_recv = 0;
+                    printf("No packet received\n");
                 }
 
                 // // Receive packets system call
