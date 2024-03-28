5ec2c3253e4cfb7129535109daae7f2ee794f01b
Doug W <doug.w@pervices.com>
2024-03-28 15:14:25 +0000

    RX_MULTI_RATE_TO_FILE: write to only 1 file + other performance changes
    Writing to 1 file only helps performance a lot

--- host/examples/rx_multi_rates_to_file.cpp
+++ host/examples/rx_multi_rates_to_file.cpp
@@ -37,6 +37,9 @@
 
 namespace po = boost::program_options;
 
+// Size of blocks of data for channel in continuous mode
+static const size_t DEFAULT_CH_BLOCK_SIZE = 1000000000;
+
 // Using uint_fast8_t instead of bool because bool causes weirdly inconsistent slowdowns
 static uint_fast8_t stop_signal_called = false;
 void sig_int_handler(int)
@@ -53,6 +56,25 @@ void free_recv_buffers(union sigval aiocb_to_free) {
     free((void*)((aiocb*)(aiocb_to_free.sival_ptr))->aio_buf);
 
     free((aiocb*)(aiocb_to_free.sival_ptr));
+}
+
+// Waits for aio writes to finish, the closes the file descriptors
+void close_aio(int output_fd) {
+    struct aiocb aiocbp_fsync;
+    memset(&aiocbp_fsync, 0, sizeof(aiocb));
+    aiocbp_fsync.aio_fildes = output_fd;
+
+    if(fsync(output_fd)) {
+        UHD_LOG_ERROR("RX_MULTI_RATES_TO_FILE", "fsync failed with " + std::string(strerror(errno)));
+    }
+
+    // Wait for aio writes to be written to disk
+    if(aio_fsync(O_SYNC, &aiocbp_fsync)) {
+        UHD_LOG_ERROR("RX_MULTI_RATES_TO_FILE", "aio_fsync failed with " + std::string(strerror(errno)));
+    }
+
+    //Closes data file
+    close(output_fd);
 }
 
 
@@ -142,34 +164,9 @@ public:
 
 bool disk_rate_message_printed = false;
 
-void receive_function(uhd::usrp::multi_usrp *usrp, channel_group *group_info, size_t spb, std::string folder, bool skip_save, bool strict) {
-    uhd::stream_args_t rx_stream_args("sc16");
-    rx_stream_args.channels = group_info->channels;
-    uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(rx_stream_args);
-
-    // Open files to write data to
-    std::vector<int> output_fd(group_info->channels.size());
-    if(!skip_save) {
-        for(size_t n = 0; n < group_info->channels.size(); n++) {
-            std::string path = folder + "/rx_ch_" + std::to_string(group_info->channels.at(n)) + ".dat";
-            output_fd[n] = open(path.c_str(), O_CREAT | O_WRONLY | O_LARGEFILE | O_TRUNC, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
-            if(output_fd[n] == -1) {
-                UHD_LOG_ERROR("RX_MULTI_RATES_TO_FILE", "Unable to open file: " + path + ". Failed with error code: " + strerror(errno));
-                std::exit(errno);
-            }
-
-            // Allocates/advises kernel for write optimizations if the amount to of data required is known
-            if(group_info->common_nsamps_requested != 0) {
-                int64_t bytes_to_allocate = group_info->common_nsamps_requested * sizeof(std::complex<short>);
-
-                int r = posix_fallocate64(output_fd[n], 0, bytes_to_allocate);
-                if(r!=0) {
-                    UHD_LOG_ERROR("RX_MULTI_RATES_TO_FILE", "Unable to allocate file: " + path + ". Failed with error code: " + strerror(errno));
-                    std::exit(errno);
-                }
-            }
-        }
-    }
+// output_fd: file descriptor to where to save data to
+// total_num_channels: total number of channels in use (not just the number of channels in this thread. Used for calculating where to store data in continuous mode
+void receive_function(uhd::rx_streamer *rx_stream, channel_group *group_info, size_t spb, bool skip_save, bool strict, uint_fast8_t continuous_mode, int output_fd, off_t output_offset, size_t total_num_channels, size_t *num_samples_received) {
 
     // Metadata return struct
     uhd::rx_metadata_t md;
@@ -181,20 +178,21 @@ void receive_function(uhd::usrp::multi_usrp *usrp, channel_group *group_info, si
     stream_cmd.time_spec  = uhd::time_spec_t(group_info->common_start_time_delay);
     rx_stream->issue_stream_cmd(stream_cmd);
 
-    size_t num_samples_received = 0;
-    size_t num_writes_issued = 0;
     bool overflow_occured = false;
     // Receive loop
-    while(!stop_signal_called && (num_samples_received < group_info->common_nsamps_requested || group_info->common_nsamps_requested == 0)) {
+    while(!stop_signal_called && (*num_samples_received < group_info->common_nsamps_requested || group_info->common_nsamps_requested == 0)) {
         // Initializes the to store rx data in
         std::vector<void*>buffer_ptrs(group_info->channels.size());
         struct aiocb* aiocb_info[group_info->channels.size()];
 
         size_t samples_this_recv;
-        if(group_info->common_nsamps_requested != 0) {
-            samples_this_recv = std::min(spb, group_info->common_nsamps_requested - num_samples_received);
+        size_t block;
+        if(!continuous_mode) {
+            block = 0;
+            samples_this_recv = std::min(spb, group_info->common_nsamps_requested - *num_samples_received);
         } else {
-            samples_this_recv = spb;
+            block = (size_t) (*num_samples_received / DEFAULT_CH_BLOCK_SIZE);
+            samples_this_recv = std::min(spb, DEFAULT_CH_BLOCK_SIZE - (*num_samples_received - (block * DEFAULT_CH_BLOCK_SIZE)));
         }
 
         // Pointers to where to store data for each channel
@@ -211,10 +209,11 @@ void receive_function(uhd::usrp::multi_usrp *usrp, channel_group *group_info, si
             aiocb_info[ch] = (aiocb*) malloc(sizeof(aiocb));
             memset(aiocb_info[ch], 0, sizeof(aiocb));
 
-            aiocb_info[ch]->aio_fildes = output_fd[ch];
-            aiocb_info[ch]->aio_offset = num_samples_received * sizeof(std::complex<short>);
-            // By giving ascending priority the aio schedueler is able to more efficiently order writes
-            aiocb_info[ch]->aio_reqprio = num_writes_issued;
+            aiocb_info[ch]->aio_fildes = output_fd;
+
+            aiocb_info[ch]->aio_offset = (*num_samples_received * sizeof(std::complex<short>)) + (ch * sizeof(std::complex<short>) * group_info->common_nsamps_requested) + output_offset + (block * sizeof(std::complex<short>) * total_num_channels);
+
+            aiocb_info[ch]->aio_reqprio = 0;
             aiocb_info[ch]->aio_buf = buffer_ptrs[ch];
 
             aiocb_info[ch]->aio_sigevent.sigev_notify = SIGEV_THREAD;
@@ -229,7 +228,7 @@ void receive_function(uhd::usrp::multi_usrp *usrp, channel_group *group_info, si
 
         if(md.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE) {
             if (md.error_code == uhd::rx_metadata_t::ERROR_CODE_TIMEOUT) {
-                fprintf(stderr, "Timeout received after %lu samples\n", num_samples_received);
+                fprintf(stderr, "Timeout received after %lu samples\n", *num_samples_received);
                 // Break if overflow occured and a set number of samples was requested, since that probably means all samples were sent, and missed ones weren't counted
                 if(strict || (overflow_occured && group_info->common_nsamps_requested !=0)) {
                     // Set to true so other threads know to stop
@@ -240,7 +239,7 @@ void receive_function(uhd::usrp::multi_usrp *usrp, channel_group *group_info, si
                 }
             } else if(md.error_code == uhd::rx_metadata_t::ERROR_CODE_OVERFLOW) {
                 if(!overflow_occured) {
-                    fprintf(stderr, "Overflow occured after %lu samples\n", num_samples_received);
+                    fprintf(stderr, "Overflow occured after %lu samples\n", *num_samples_received);
                     overflow_occured = true;
                     if(strict) {
                         // Set to true so other threads know to stop
@@ -253,7 +252,7 @@ void receive_function(uhd::usrp::multi_usrp *usrp, channel_group *group_info, si
             }
         }
 
-        num_samples_received += num_samples;
+        *num_samples_received += num_samples;
 
         if(!skip_save) {
             bool write_failed = false;
@@ -267,7 +266,6 @@ void receive_function(uhd::usrp::multi_usrp *usrp, channel_group *group_info, si
                     free(buffer_ptrs[ch]);
                     free(aiocb_info[ch]);
                     write_failed = true;
-                    num_writes_issued++;
                 }
             }
             // Exit recv loop if any of the writes failed and strict mode
@@ -292,33 +290,6 @@ void receive_function(uhd::usrp::multi_usrp *usrp, channel_group *group_info, si
     if(group_info->common_nsamps_requested == 0) {
         stream_cmd.stream_mode = uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS;
         rx_stream->issue_stream_cmd(stream_cmd);
-    }
-
-    printf("Waiting for remaing writes to finish\n");
-
-    if(!skip_save) {
-        // DEBUG: lock to prevent overlapping fsyncs (may be causing segmentation faults)
-        // std::lock_guard<std::mutex> guard(fsync_mutex);
-        for(size_t ch = 0; ch < group_info->channels.size(); ch++) {
-            struct aiocb aiocbp_fsync;
-            memset(&aiocbp_fsync, 0, sizeof(aiocb));
-            aiocbp_fsync.aio_fildes = output_fd[ch];
-
-            if(fsync(output_fd[ch])) {
-                UHD_LOG_ERROR("RX_MULTI_RATES_TO_FILE", "fsync failed with " + std::string(strerror(errno)));
-            }
-
-            // Wait for aio writes to be written to disk
-            if(aio_fsync(O_SYNC, &aiocbp_fsync)) {
-                UHD_LOG_ERROR("RX_MULTI_RATES_TO_FILE", "aio_fsync failed with " + std::string(strerror(errno)));
-                printf("output_fd[%lu]: %i\n", group_info->channels[ch], output_fd[ch]);
-            }
-        }
-    }
-
-    //Closes data files
-    for(uint64_t n = 0; n < output_fd.size(); n++) {
-        close(output_fd[n]);
     }
 }
 
@@ -388,6 +359,16 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
 
     std::vector<size_t> nsamps = parse_argument<size_t>(nsamp_arg, channels.size());
 
+    // Check if continuous mode is being used and check to make sure user did not request continuous and nsamps mode
+    uint_fast8_t continuous_mode = nsamps[0] == 0;
+    for(size_t n = 1; n < nsamps.size(); n++) {
+        bool other_continuous_mode = nsamps[n]==0;
+        if(continuous_mode != other_continuous_mode) {
+            UHD_LOG_ERROR("RX_MULTI_RATES_TO_FILE", "Mixing continuous and nsamps modes not allowed");
+            return EINVAL;
+        }
+    }
+
     std::vector<channel_group> groups;
 
     for(size_t ch_i = 0; ch_i < channels.size(); ch_i++) {
@@ -404,22 +385,171 @@ int UHD_SAFE_MAIN(int argc, char* argv[])
         }
     }
 
+    // It is faster to save all data to one file than multiple
+    // Data is saved to an intermediate file, then later copied to individual files
+    int intermediate_fd = -1;
+    std::string path = folder + "/tmp.dat";
+    if(!skip_save) {
+        std::filesystem::create_directories(folder);
+        intermediate_fd = open(path.c_str(), O_CREAT | O_DIRECT | O_WRONLY | O_LARGEFILE, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+
+        if(intermediate_fd == -1) {
+            UHD_LOG_ERROR("RX_MULTI_RATES_TO_FILE", "Unable to open file: " + path + ". Failed with error code: " + strerror(errno));
+            std::exit(errno);
+        }
+
+        // Allocates/advises kernel for write optimizations if the amount to of data required is known
+        int64_t bytes_to_allocate;
+        if(continuous_mode) {
+            // Exact amount of space to allocate unknown, optimize of DEFAULT_CH_BLOCK_SIZE samples
+            bytes_to_allocate = DEFAULT_CH_BLOCK_SIZE * sizeof(std::complex<short>) * channels.size();
+        } else {
+            // Allocate the exact amount of data needed to store all requested samples
+            bytes_to_allocate = 0;
+            for(size_t n = 0; n < groups.size(); n++) {
+                bytes_to_allocate+= groups[n].common_nsamps_requested * sizeof(std::complex<short>) * groups[n].channels.size();
+            }
+        }
+
+        int r = fallocate(intermediate_fd, FALLOC_FL_ZERO_RANGE, 0, bytes_to_allocate);
+        if(r!=0) {
+            UHD_LOG_ERROR("RX_MULTI_RATES_TO_FILE", "Unable to allocate file: " + path + ". Failed with error code: " + strerror(errno));
+            std::exit(errno);
+        }
+        posix_fadvise(intermediate_fd, 0, bytes_to_allocate, POSIX_FADV_DONTNEED);
+    }
+
+    std::vector<std::thread> receive_threads;
+
     std::cout << ("Setting device timestamp to 0") << std::endl;
     usrp->set_time_now(uhd::time_spec_t(0.0));
 
-    if(!skip_save) {
-        std::filesystem::create_directories(folder);
-    }
+    std::vector<uhd::rx_streamer::sptr> rx_streamers;
 
     std::signal(SIGINT, &sig_int_handler);
 
-    std::vector<std::thread> receive_threads;
+
+    std::cout << ("Starting receive") << std::endl;
+
+    size_t current_offset = 0;
+    // Stores total number of samples received
+    std::vector<size_t> num_samples_received(groups.size(), 0);
     for(size_t n = 0; n < groups.size(); n++) {
-        receive_threads.emplace_back(std::thread(receive_function, usrp.get(), &groups[n], spb, folder, skip_save, strict));
+
+        uhd::stream_args_t rx_stream_args("sc16");
+        rx_stream_args.channels = groups[n].channels;
+        uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(rx_stream_args);
+        rx_streamers.push_back(rx_stream);
+
+        receive_threads.emplace_back(std::thread(receive_function, rx_streamers[n].get(), &groups[n], spb, skip_save, strict, continuous_mode, intermediate_fd, current_offset, channels.size(), &num_samples_received[n]));
+
+        size_t block_size;
+        if(continuous_mode) {
+            block_size = DEFAULT_CH_BLOCK_SIZE;
+        } else {
+            block_size = groups[n].common_nsamps_requested;
+        }
+
+        current_offset+= groups[n].channels.size() * block_size * sizeof(std::complex<short>);
     }
 
     for(auto& receive_thread : receive_threads) {
         receive_thread.join();
+    }
+
+    if(!skip_save) {
+        // Wait for aio to finish and close the intermediate file
+        close_aio(intermediate_fd);
+
+        // Re-open the data was stored in during receive_function
+        // For performance reasons the file was previously opened with settings optimized for writing that prevent reading for receive_function
+        intermediate_fd = open(path.c_str(), O_RDONLY | O_LARGEFILE, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+        if(intermediate_fd == -1) {
+            fprintf(stderr, "errnor %s when attempting to open intermediate file for copying. Data collected is stored in %s\n", strerror(errno), path.c_str());
+            return errno;
+        }
+
+        // File descriptor of the final files to store data in
+        int final_fds[channels.size()];
+        // List of channels in the order they appear in their data is stored
+        size_t channel_list[channels.size()];
+        size_t channel_list_index = 0;
+        for(size_t group_i = 0; group_i < groups.size(); group_i++) {
+            for(size_t ch_i = 0; ch_i < groups[group_i].channels.size(); ch_i++) {
+                channel_list[channel_list_index] = groups[group_i].channels[ch_i];
+                channel_list_index++;
+            }
+        }
+
+        for(size_t n = 0; n < channels.size(); n++) {
+            std::string ch_path = folder + "/rx_ch_" + std::to_string(channel_list[n]) + ".dat";
+            final_fds[n] = open(path.c_str(), O_CREAT | O_WRONLY | O_LARGEFILE | O_TRUNC, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+        }
+
+        size_t abs_ch_i = 0;
+        // Offset for where the data start for the next channel
+        size_t ch_offset = 0;
+
+        for(size_t group_i = 0; group_i < groups.size(); group_i++) {
+            // Buffer to use when copying data from intermediate file to individual files
+            std::vector<uint8_t> buffer(DEFAULT_CH_BLOCK_SIZE * sizeof(std::complex<short>));
+            size_t samples_to_copy = num_samples_received[group_i];
+
+            for(size_t ch_i = 0; ch_i < groups[group_i].channels.size(); ch_i++) {
+                size_t samples_copied = 0;
+
+                while(samples_copied < samples_to_copy) {
+                    size_t target_samples_this_copy;
+                    size_t offset;
+
+                    if(!continuous_mode) {
+                        target_samples_this_copy = std::min(DEFAULT_CH_BLOCK_SIZE, samples_to_copy - samples_copied);
+                        offset = (ch_offset + samples_copied) * sizeof(std::complex<short>);
+                    } else {
+                        size_t sample_copied_this_block = samples_copied % DEFAULT_CH_BLOCK_SIZE;
+                        size_t sample_remaining_this_block = std::min(DEFAULT_CH_BLOCK_SIZE - sample_copied_this_block, samples_to_copy - samples_copied);
+
+                        target_samples_this_copy = std::min(DEFAULT_CH_BLOCK_SIZE, sample_remaining_this_block);;
+
+                        size_t block = samples_copied / DEFAULT_CH_BLOCK_SIZE;
+                        offset = ((block * DEFAULT_CH_BLOCK_SIZE) + (sample_copied_this_block) + ch_offset) * sizeof(std::complex<short>);
+                    }
+                    ssize_t data_read = pread(final_fds[abs_ch_i], buffer.data(), target_samples_this_copy * sizeof(std::complex<short>), offset);
+
+                    if(data_read == 1) {
+                        fprintf(stderr, "read error %s while attempting to copy data from ch %lu to its own file\n", strerror(errno), groups[group_i].channels[ch_i]);
+                        break;
+                    }
+
+                    samples_copied += (data_read / sizeof(std::complex<short>));
+
+                    size_t ret = write(final_fds[abs_ch_i], buffer.data(), data_read);
+                    if(ret == 0) {
+                        fprintf(stderr, "write error %s while attempting to copy data from ch %lu to its own file\n", strerror(errno), groups[group_i].channels[ch_i]);
+                        break;
+                    }
+                }
+
+                abs_ch_i++;
+
+                if(!continuous_mode) {
+                    ch_offset += groups[group_i].common_nsamps_requested;
+                } else {
+                    ch_offset += DEFAULT_CH_BLOCK_SIZE;
+                }
+            }
+        }
+
+
+        close(intermediate_fd);
+        for(size_t n = 0; n < channels.size(); n++) {
+            close(final_fds[n]);
+        }
+
+        // Deletes temporary file containing all data
+        // Will only delete the file, never the folder
+        unlink(path.c_str());
+
     }
 
     // finished
