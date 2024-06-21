338b4e3fecc157435877fefef9ef61c75fb60e61
Doug W <doug.w@pervices.com>
2024-06-21 22:38:36 +0000

    re-enable M_ARENA_MAX disable M_TRIM_THRESHOLD

--- host/lib/usrp/multi_usrp.cpp
+++ host/lib/usrp/multi_usrp.cpp
@@ -148,14 +148,16 @@ public:
         }
         // Disables trimming
         // I think this removes the costs deallocating unused memory but means that hte program will always be increasing in size
-        r = mallopt(M_TRIM_THRESHOLD, -1);
-        if(r == 0) {
-            printf("mallopt M_TRIM_THRESHOLD error\n");
-        }
-        // r = mallopt(M_ARENA_MAX, 384);
+        // r = mallopt(M_TRIM_THRESHOLD, -1);
         // if(r == 0) {
-        //     printf("mallopt M_ARENA_MAX error\n");
+        //     printf("mallopt M_TRIM_THRESHOLD error\n");
         // }
+        // Experimentally proven to improve performance
+        // TODO: soft code this, the limit 8 * number of cores
+        r = mallopt(M_ARENA_MAX, 384);
+        if(r == 0) {
+            printf("mallopt M_ARENA_MAX error\n");
+        }
         _tree = _dev->get_tree();
     }
 
