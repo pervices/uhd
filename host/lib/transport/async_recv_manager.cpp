c38bc20b995886e523b25daec69d30120f6609f8
Doug W <doug.w@pervices.com>
2024-08-07 18:28:20 +0000

    debug msg

--- host/lib/transport/async_recv_manager.cpp
+++ host/lib/transport/async_recv_manager.cpp
@@ -242,7 +242,7 @@ uint8_t* async_recv_manager::get_next_packet_vita_header(const size_t ch) {
     size_t b = active_consumer_buffer[ch];
     uint8_t* addr = access_vita_hdr(ch, 0, b, num_packets_consumed[ch]);
     if(*access_num_packets_stored(ch, 0, b) > num_packets_consumed[ch]) {
-        printf("*access_num_packets_stored(%lu, 0, %lu): %li\n", ch, b, *access_num_packets_stored(ch, 0, b));
+        printf("1 *access_num_packets_stored(%lu, 0, %lu): %li\n", ch, b, *access_num_packets_stored(ch, 0, b));
         printf("num_packets_consumed[ch]: %lu\n", num_packets_consumed[ch]);
         return addr;
     }
@@ -271,7 +271,7 @@ void async_recv_manager::advance_packet(const size_t ch) {
     int_fast64_t* num_packets_stored_addr = access_num_packets_stored(ch, 0, b);
     if(num_packets_consumed[ch] >= *num_packets_stored_addr) [[unlikely]] {
         printf("num_packets_consumed[ch]: %lu\n", num_packets_consumed[ch]);
-        printf("*num_packets_stored_addr: %li\n", *num_packets_stored_addr);
+        printf("50 *num_packets_stored_addr: %li\n", *num_packets_stored_addr);
         if(*num_packets_stored_addr == 0) {
             throw std::runtime_error("Advancing buffer despite no samples in it");
         }
