694ab051a4211d45d2a70f7d345b35c8f769013b
Doug W <doug.w@pervices.com>
2024-08-07 18:29:38 +0000

    debug msg

--- host/lib/transport/super_recv_packet_handler_mmsg.cpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.cpp
@@ -296,6 +296,8 @@ public:
                 }
             }
 
+            std::cout << "40 *access_num_packets_stored(0, 0, 0): " << *recv_manager->access_num_packets_stored(0, 0, 0) << std::endl;
+
             if(realignment_required) [[unlikely]] {
                 if(realignment_attempts >= max_realignment_attempts) {
                     if(!align_message_printed) {
