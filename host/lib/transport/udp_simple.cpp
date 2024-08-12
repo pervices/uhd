880772cc73f6354c40e4d3a96bb397540fb53bf7
Doug W <doug.w@pervices.com>
2024-08-12 18:01:16 +0000

    UDP_SIMPLE: adding mutex
    Theoretically not needed, but without it there is sometimes a "resource
    temporarily unavailable error"
    
    Also disable the pps monitor thread (since it is not ready in the FPGA
    and causes spurious warnings) and set a default value for timestamps in
    rx stream commands

--- host/lib/transport/udp_simple.cpp
+++ host/lib/transport/udp_simple.cpp
@@ -71,14 +71,18 @@ public:
 
     size_t send(const void* buff, size_t count) override
     {
+        std::lock_guard<std::mutex> guard(send_mutex);
         if (_connected) {
             // MSG_CONFIRM to avoid uneccessary control packets being sent to verify the destination is where it already is
-            ssize_t data_sent = ::send(socket_fd, buff, count, MSG_CONFIRM & route_good);
+            ssize_t data_sent = ::send(socket_fd, buff, count, MSG_DONTWAIT | (MSG_CONFIRM & route_good));
             if(data_sent == -1) {
                 UHD_LOG_ERROR("UDP", "Attempt to send UDP control packet failed with: " + std::string(strerror(errno)));
                 return 0;
-            } else {
+            } else if((size_t) data_sent == count) {
                 return data_sent;
+            } else {
+                UHD_LOG_ERROR("UDP", "Mistmatch between data sent and data attempted to be sent during send");
+                return 0;
             }
         }
 
@@ -89,13 +93,15 @@ public:
         dst_address.sin_addr.s_addr = inet_addr(ipv4_addr.c_str());
         dst_address.sin_port = htons(_send_endpoint.port());
 
-        ssize_t ret = sendto(socket_fd, buff, count, MSG_CONFIRM & route_good, (struct sockaddr*)&dst_address, sizeof(dst_address));
+        ssize_t ret = sendto(socket_fd, buff, count, MSG_DONTWAIT | (MSG_CONFIRM & route_good), (struct sockaddr*)&dst_address, sizeof(dst_address));
 
-        if(ret > 0) {
+        if(ret == -1) {
+            UHD_LOG_ERROR("UDP", "Attempt to send UDP control packet failed with: " + std::string(strerror(errno)));
+            return 0;
+        } else if((size_t) ret == count) {
             return ret;
         } else {
-            UHD_LOG_ERROR("UDP", "Attempt to sendto UDP control packet failed with: " + std::string(strerror(errno)));
-            // Return 0 to keep behaviour from asio
+            UHD_LOG_ERROR("UDP", "Mistmatch between data sent and data attempted to be sent during sendto");
             return 0;
         }
     }
@@ -108,6 +114,7 @@ public:
 
     size_t recv(void* buff, size_t size, double timeout) override
     {
+        std::lock_guard<std::mutex> guard(recv_mutex);
         const int32_t timeout_ms = static_cast<int32_t>(timeout * 1000);
 
         if (not wait_for_recv_ready(socket_fd, timeout_ms)) {
@@ -165,6 +172,9 @@ private:
     // If this has been confirmed send can be called with MSG_CONFIRM
     int route_good = 0;
     int socket_fd;
+    // Usually no mutex is required with socket operations, however sometimes during heavy network activity (such as the continuously running receive loops) not using a mutex results in "resource temporarily unavailable errors"
+    std::mutex send_mutex;
+    std::mutex recv_mutex;
 };
 
 udp_simple::~udp_simple(void)
