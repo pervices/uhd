463d91a97cf0d313ae29404833fce302daf9d24f
Doug W <doug.w@pervices.com>
2024-10-15 19:55:54 +0000

    moving lfence

--- host/lib/transport/super_recv_packet_handler_mmsg.cpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.cpp
@@ -436,9 +436,9 @@ public:
                 packet_infos[ch].packet_hdr = recv_manager->get_next_packet_vita_header(ch);
                 // Increment the channel count when packet_infos[ch].packet_hdr is not null (!! turns any non 0 value into 1);
                 ch += !!packet_infos[ch].packet_hdr;
-            }
 
-            _mm_lfence();
+                _mm_lfence();
+            }
 
             // Check if timeout occured
             // TODO: refactor to be branchless
