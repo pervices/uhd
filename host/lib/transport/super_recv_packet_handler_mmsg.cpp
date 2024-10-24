659bb573e5e1ec2c9db784ed485e8a4bd2b90811
Doug W. <doug.w@pervices.com>
2024-10-24 17:35:02 +0000

    DEBUG: moving where header copy is obtained

--- host/lib/transport/super_recv_packet_handler_mmsg.cpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.cpp
@@ -302,6 +302,8 @@ public:
                 initial_buffer_write_count[ch] = recv_manager->get_buffer_write_count(ch);
                 // if (buffer_write_count has increased since the last recv || the next packet is not the first packet of the buffer) && buffer_write_count is even
                 if((initial_buffer_write_count[ch] > _previous_buffer_write_count[ch] || !recv_manager->is_first_packet_of_buffer(ch)) && !(initial_buffer_write_count[ch] & 1)) {
+                    // Create copy of header for this packet
+                    memcpy(packet_infos[ch].packet_hdr, recv_manager->get_next_packet_vita_header(ch), _HEADER_SIZE);
                     // Move onto the next channel since this one is ready
                     ch++;
                 } else {
@@ -330,8 +332,6 @@ public:
             bool mid_header_read_header_overwrite = false;
 
             for(size_t ch = 0; ch < _NUM_CHANNELS; ch++) {
-                // Gets info for this packet
-                memcpy(packet_infos[ch].packet_hdr, recv_manager->get_next_packet_vita_header(ch), _HEADER_SIZE);
 
                 packet_infos[ch].packet_samples = recv_manager->get_next_packet_samples(ch);
                 packet_infos[ch].packet_length = recv_manager->get_next_packet_length(ch);
