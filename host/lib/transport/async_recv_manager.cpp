3b6a38e69db96b96c0293318726a5ddba19233fb
Doug W. <doug.w@pervices.com>
2024-11-08 15:52:11 +0000

    DEBUG: disable madvise for vita hdr

--- host/lib/transport/async_recv_manager.cpp
+++ host/lib/transport/async_recv_manager.cpp
@@ -66,7 +66,7 @@ flush_complete((uint8_t*) aligned_alloc(cache_line_size, _num_ch * padded_uint_f
             // Hint to keep the mmsghdrs/iovecs, vita headers in cache
             // Probably doesn't actually do anything
             madvise(access_mmsghdr_buffer(ch, 0, b), _num_packets_stored_times_written_mmmsghdr_iovec_subbuffer_size, MADV_WILLNEED);
-            madvise(access_vita_hdr(ch, 0, b, 0), _vitahdr_subbuffer_size, MADV_WILLNEED);
+            // madvise(access_vita_hdr(ch, 0, b, 0), _vitahdr_subbuffer_size, MADV_WILLNEED);
         }
     }
 
