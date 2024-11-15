d145d532991d5c1d6189d546a6ef9aeaab038d1f
Doug W. <doug.w@pervices.com>
2024-11-15 20:23:39 +0000

    Setting busy poll socket option

--- host/lib/transport/super_recv_packet_handler_mmsg.cpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.cpp
@@ -136,6 +136,12 @@ public:
                 fprintf(stderr, "Attempting to set rx socket priority failed with error code: %s", strerror(errno));
             }
 
+            const int busy_poll_time = 1000;
+            int set_busy_poll_ret = setsockopt(_recv_sockets[n], SOL_SOCKET, SO_BUSY_POLL, &busy_poll_time, sizeof(set_busy_poll_ret));
+            if(set_priority_ret) {
+                fprintf(stderr, "Attempting to set rx busy read priority failed with error code: %s", strerror(errno));
+            }
+
             // recvmmsg should attempt to recv at most the amount to fill 1/_NUM_CHANNELS of the socket buffer
             _MAX_PACKETS_TO_RECV = (int)((_ACTUAL_RECV_BUFFER_SIZE/(_NUM_CHANNELS + 1))/(_HEADER_SIZE + _MAX_SAMPLE_BYTES_PER_PACKET + 42));
         }
