ba7069233b4df1465e6bf906f1465a578d88602a
Doug W. <doug.w@pervices.com>
2024-11-20 21:04:27 +0000

    Re-enable pps

--- host/lib/transport/super_recv_packet_handler_mmsg.cpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.cpp
@@ -206,8 +206,6 @@ public:
             size_t recv_manager_size = (size_t) ceil(sizeof(async_recv_manager) / (double)getpagesize()) * getpagesize();
             recv_manager = (async_recv_manager*) aligned_alloc(getpagesize(), recv_manager_size);
             new (recv_manager) async_recv_manager(device_total_rx_channels, recv_sockets, header_size, max_sample_bytes_per_packet, device_total_rx_channels);
-
-            // madvise(recv_manager, recv_manager_size, MADV_NOHUGEPAGE);
         }
     }
 
