1c447563243eeec259251e7416590df460e408fc
Doug W. <doug.w@pervices.com>
2024-11-20 16:51:53 +0000

    madvise no huge page, undoing padding to huge page

--- host/lib/transport/async_recv_manager.cpp
+++ host/lib/transport/async_recv_manager.cpp
@@ -31,7 +31,7 @@ _data_subbuffer_size((size_t) std::ceil((PACKETS_PER_BUFFER * _packet_data_size)
 // Size of the buffer for each channel
 // Padded so that each buffer is on their own huge page. This prevents rare stalls
 // TODO: optimize this to avoid padding ~20kB to 2MB. Maybe using madvise no huge page fixes it
-_combined_buffer_size(std::ceil((_mmmsghdr_iovec_subbuffer_size + _vitahdr_subbuffer_size + _data_subbuffer_size) / (double) HUGE_PAGE_SIZE) * HUGE_PAGE_SIZE ),
+_combined_buffer_size(std::ceil((_mmmsghdr_iovec_subbuffer_size + _vitahdr_subbuffer_size + _data_subbuffer_size) / (double) PAGE_SIZE) * PAGE_SIZE ),
 // Allocates buffer to store all mmsghdrs, iovecs, Vita headers, Vita payload
 // MAP_HUGETLB is meant to tell it to use huge pages, but it causing it to fail
 _combined_buffer((uint8_t*) mmap(nullptr, _num_ch * NUM_BUFFERS * _combined_buffer_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | /*MAP_HUGETLB |*/ MAP_ANONYMOUS, -1, 0)),
@@ -52,6 +52,8 @@ flush_complete((uint8_t*) aligned_alloc(CACHE_LINE_SIZE, _num_ch * padded_uint_f
     if(_combined_buffer == MAP_FAILED) {
         throw uhd::environment_error( "Failed to allocate internal buffer" );
     }
+
+    madvise(_combined_buffer, _num_ch * NUM_BUFFERS * _combined_buffer_size, MADV_NOHUGEPAGE);
 
     // Create buffers used to store control data for the consumer thread
     size_t active_consumer_buffer_size = _num_ch * sizeof(size_t);
