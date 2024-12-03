f3e1689224c287db0027fadf22b12e589c4fd83c
Doug W. <doug.w@pervices.com>
2024-12-03 16:27:14 +0000

    SEND: always send 1 dummy sample in an eob packet
    Previously we would send the minimum number of samples for a packet with
    actual samples. The FPGA handles eob packets with 1 dummy sample better
    than a packet with _DEVICE_NSAMPS_MULTIPLE samples

--- host/lib/transport/super_send_packet_handler_mmsg.cpp
+++ host/lib/transport/super_send_packet_handler_mmsg.cpp
@@ -697,9 +697,11 @@ private:
 
     void send_eob_packet(const uhd::tx_metadata_t &metadata, double timeout) {
 
+        // How many dummy samples to send in the eob
+        constexpr size_t dummy_samples_in_eob = 1;
 
         // Create vector of dummy samples, since the FPGA cannot handle 0 sample packets
-        std::vector<std::vector<int8_t>> dummy_buffs(_NUM_CHANNELS, std::vector<int8_t>(_BYTES_PER_SAMPLE * _DEVICE_PACKET_NSAMP_MULTIPLE, 0));
+        std::vector<std::vector<int8_t>> dummy_buffs(_NUM_CHANNELS, std::vector<int8_t>(_BYTES_PER_SAMPLE * dummy_samples_in_eob, 0));
         std::vector<const void *> dummy_buff_ptrs;
         for(size_t n = 0; n < _NUM_CHANNELS; n++) {
             dummy_buff_ptrs.push_back(dummy_buffs[n].data());
@@ -724,7 +726,7 @@ private:
         nsamps_in_cache = 0;
 
         // Sends the eob packet
-        send_multiple_packets(dummy_buff_ptrs, _DEVICE_PACKET_NSAMP_MULTIPLE, eob_md, timeout, true);
+        send_multiple_packets(dummy_buff_ptrs, dummy_samples_in_eob, eob_md, timeout, true);
     }
 
     /*!
