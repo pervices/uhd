a67643242bbfd3382a6882b8a150c2bb256362b4
Doug W. <doug.w@pervices.com>
2024-12-06 21:11:22 +0000

    DEBUG: never have consumer check provider

--- host/lib/transport/super_recv_packet_handler_mmsg.cpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.cpp
@@ -262,21 +262,21 @@ public:
             size_t ch = 0;
             // While not all channels have been obtained and timeout has not been reached
             while(ch < _NUM_CHANNELS && recv_start_time + timeout > get_system_time()) {
-                recv_manager->get_next_async_packet_info(ch, &next_packet[ch]);
+                // recv_manager->get_next_async_packet_info(ch, &next_packet[ch]);
 
                 // Adding excesive amount of mfences to avoid wierd errors
                 // TODO: optimize fencing
                 _mm_mfence();
 
                 // Length is 0 if the packet is not ready yet
-                if(next_packet[ch].length != 0) {
+                // if(next_packet[ch].length != 0) {
                     // Move onto the next channel since this one is ready
                     // DEBUG: isolating seg fault
                     // ch++;
-                } else {
+                // } else {
                     // Do nothing
                     // _mm_pause (which marks this as a polling loop) might help, but it appears to make performance worse
-                }
+                // }
                 _mm_mfence();
             }
             // This mmfence helped prevent wierd performance drops with the old system
