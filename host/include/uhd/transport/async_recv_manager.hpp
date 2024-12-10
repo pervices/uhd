5cad14f2fcdee9fc282b56c1ee883c0f77490119
Doug W. <doug.w@pervices.com>
2024-12-10 16:54:51 +0000

    DBUG: adding mfence after advancing buffers

--- host/include/uhd/transport/async_recv_manager.hpp
+++ host/include/uhd/transport/async_recv_manager.hpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <liburing.h>
 #include <uhd/utils/log.hpp>
+#include <immintrin.h>
 
 namespace uhd { namespace transport {
 
@@ -229,6 +230,9 @@ public:
         //     packets_advancable = 0;
         // }
         io_uring_buf_ring_cq_advance(ring, *access_io_uring_buf_rings(ch, 0), 1);
+
+        // Maybe helps push updates to the ring faster
+        _mm_mfence();
     }
 
 
