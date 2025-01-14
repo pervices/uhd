eed953659075f94a7fe150dc75a9568290181ad8
Doug W. <doug.w@pervices.com>
2025-01-14 22:35:18 +0000

    continuing

--- host/lib/utils/network_config.cpp
+++ host/lib/utils/network_config.cpp
@@ -14,42 +14,49 @@
 #include <arpa/inet.h>
 
 std::string uhd::get_dev_from_ipv4(std::string ipv4) {
-    struct ifaddrs *ifaddr;
+    // Start of a linked list containing info for each network interface
+    struct ifaddrs *ifaddr_start;
 
     // Gets a linked list of information for all network interfaces
-    int r = getifaddrs(&ifaddr);
+    int r = getifaddrs(&ifaddr_start);
     if(r == -1) {
         UHD_LOG_ERROR("NETWORK_CONFIG", "Unable to get device from ip. \"getifaddrs\" failed with error code: " + std::string(strerror(errno)));
         throw uhd::system_error("getifaddrs: " + std::string(strerror(errno)));
     }
 
     // Cycle through every element of the interface list
-    while (ifaddr != NULL) {
+    // Current position on the list
+    struct ifaddrs *ifaddr_head = ifaddr_start;
+    while (ifaddr_head != NULL) {
 
-        if(ifaddr->ifa_addr->sa_family != AF_INET) {
+        if(ifaddr_head->ifa_addr->sa_family != AF_INET) {
             //Skip non IPV4 addresses
         } else {
             // Buffer to store ip
             char ip_buff[INET_ADDRSTRLEN];
-            struct sockaddr_in *sa = (struct sockaddr_in *) ifaddr->ifa_addr;
+            struct sockaddr_in *sa = (struct sockaddr_in *) ifaddr_head->ifa_addr;
             // Extract ip
             const char* ip_buffer_r = inet_ntop(AF_INET, &sa->sin_addr, ip_buff, INET_ADDRSTRLEN);
 
             // Check if the ip address was extracted successfully. It should be impossible to this to fail
             if(ip_buffer_r != ip_buff) {
-                throw uhd::runtime_error( "Failed to parse ipv4 address for device: " + std::string(ifaddr->ifa_name));
+                freeifaddrs(ifaddr_start);
+                throw uhd::runtime_error( "Failed to parse ipv4 address for device: " + std::string(ifaddr_head->ifa_name));
             }
 
-            printf("ifa_name: %s\n", ifaddr->ifa_name);
-            printf("ip_buff: %s\n", ip_buff);
+            if(std::string(ip_buff) == ipv4) {
+                printf("ifa_name: %s\n", ifaddr_head->ifa_name);
+                printf("ip_buff: %s\n", ip_buff);
+                freeifaddrs(ifaddr_start);
+                return std::string(ifaddr_head->ifa_name);
+            }
         }
 
-
         // Advance to the next element in the linked list
-        ifaddr = ifaddr->ifa_next;
+        ifaddr_head = ifaddr_head->ifa_next;
     }
 
-    // TODO: free list
-
+    freeifaddrs(ifaddr_start);
+    // TODO: throw error
     return "";
 }
