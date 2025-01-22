b6ca7917e558c090ccff67ee9c5b78773146491e
Doug W. <doug.w@pervices.com>
2025-01-22 21:42:50 +0000

    continuing

--- host/lib/transport/super_recv_packet_handler_mmsg.cpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.cpp
@@ -15,7 +15,6 @@
 #include <uhd/exception.hpp>
 #include <uhd/stream.hpp>
 #include <uhd/transport/vrt_if_packet.hpp>
-#include <uhd/transport/user_recv_manager.hpp>
 #include <uhd/types/metadata.hpp>
 #include <uhd/utils/byteswap.hpp>
 #include <uhd/utils/log.hpp>
@@ -43,6 +42,12 @@
 
 #include <immintrin.h>
 
+#ifdef HAVE_LIBURING
+    #include <uhd/transport/io_uring_recv_manager.hpp>
+#else
+    #include <uhd/transport/user_recv_manager.hpp>
+#endif
+
 #define MIN_MTU 9000
 
 namespace uhd { namespace transport { namespace sph {
@@ -62,9 +67,9 @@ class recv_packet_handler_mmsg : public recv_packet_handler
 {
 public:
 #ifdef HAVE_LIBURING
-    #error "Enabled"
+    typedef io_uring_recv_manager recv_manager_mode;
 #else
-    #error "Not enabled"
+    typedef user_recv_manager recv_manager_mode;
 #endif
 
     /*!
@@ -175,12 +180,12 @@ public:
         }
 
         // Create manager for receive threads and access to buffer recv data
-        recv_manager = user_recv_manager::make(device_total_rx_channels, recv_sockets, header_size, max_sample_bytes_per_packet);
+        recv_manager = recv_manager_mode::make(device_total_rx_channels, recv_sockets, header_size, max_sample_bytes_per_packet);
     }
 
     ~recv_packet_handler_mmsg(void)
     {
-        user_recv_manager::unmake(recv_manager);
+        recv_manager_mode::unmake(recv_manager);
         // recv_manager must be deleted before closing sockets
         for(size_t n = 0; n < _recv_sockets.size(); n++) {
             int r = close(_recv_sockets[n]);
@@ -478,7 +483,7 @@ private:
     // The warning for using non performance governor has already been printed
     bool _performance_warning_printed = false;
 
-    user_recv_manager* recv_manager;
+    recv_manager_mode* recv_manager;
 
     // Cache of samples from packets that are leftover and stored until the next packet
     // _sample_cache in wire format
