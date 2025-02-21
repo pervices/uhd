edeccb59da90fc5b2af901bafb855929c1acaff7
Doug W. <doug.w@pervices.com>
2025-02-21 21:13:59 +0000

    CYAN: replacing clock sync atomics with sfence

--- host/lib/usrp/cyan_nrnt/cyan_nrnt_io_impl.cpp
+++ host/lib/usrp/cyan_nrnt/cyan_nrnt_io_impl.cpp
@@ -534,6 +534,8 @@ bool cyan_nrnt_impl::recv_async_msg(
 rx_streamer::sptr cyan_nrnt_impl::get_rx_stream(const uhd::stream_args_t &args_){
     // Set flag to indicate clock sync is desired so that clock sync warnings are displayed
     clock_sync_desired = true;
+    // sfence to ensure the need for clock sync is pushed to other threads
+    _mm_sfence();
 
     stream_args_t args = args_;
 
@@ -847,6 +849,8 @@ static void get_fifo_lvl_udp_abs( const size_t channel, const int64_t bl_multipl
 tx_streamer::sptr cyan_nrnt_impl::get_tx_stream(const uhd::stream_args_t &args_){
     // Set flag to indicate clock sync is desired so that clock sync warnings are displayed
     clock_sync_desired = true;
+    // sfence to ensure the need for clock sync is pushed to other threads
+    _mm_sfence();
 
     stream_args_t args = args_;
 
