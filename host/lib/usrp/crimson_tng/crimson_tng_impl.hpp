7ee3c965252f38f3a2f0d718458c74394d600f94
Doug W. <doug.w@pervices.com>
2025-02-25 20:36:35 +0000

    CRIMSON: reducing false sharing in clock sync

--- host/lib/usrp/crimson_tng/crimson_tng_impl.hpp
+++ host/lib/usrp/crimson_tng/crimson_tng_impl.hpp
@@ -90,6 +90,10 @@ class crimson_tng_impl : public uhd::device
 public:
     static constexpr uint_fast8_t NUMBER_OF_XG_CONTROL_INTF = 2;
 
+    // Cache line size
+    // Assume it is 64, which is the case for virtually all AMD64 systems
+    static constexpr uint_fast8_t CACHE_LINE_SIZE = 64;
+
     // This is the core constructor to be called when a crimson_tng device is found
     crimson_tng_impl(const uhd::device_addr_t &);
     ~crimson_tng_impl(void);
@@ -116,10 +120,10 @@ public:
     bool time_resync_requested = false;
 
     inline double time_diff_get() {
-        return _time_diff;
+        return *_time_diff;
     }
     inline void time_diff_set( double time_diff ) {
-        _time_diff = time_diff;
+        *_time_diff = time_diff;
     }
 
     void start_bm();
@@ -171,10 +175,12 @@ private:
 	 *     such that the error is forced to zero.
 	 *     => Crimson Time Now := Host Time Now + CV
 	 */
-	uhd::pidc _time_diff_pidc;
-    // TODO: make _time_diff and _time_diff_converged false-sharing proof
-    double _time_diff;
-    bool _time_diff_converged;
+    static constexpr size_t padded_pidc_tcl_size = (size_t) ceil(sizeof(uhd::pidc_tl) / (double)CACHE_LINE_SIZE) * CACHE_LINE_SIZE;
+	uhd::pidc* const _time_diff_pidc;
+    // NOTE: use _mm_sfence after writing to _time_diff or _time_diff_converged
+    // The difference in time on the device and host
+    double* const _time_diff;
+    bool _time_diff_converged = false;
 	uhd::time_spec_t _streamer_start_time;
     void time_diff_send( const uhd::time_spec_t & crimson_now );
     bool time_diff_recv( time_diff_resp & tdr );
