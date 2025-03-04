b7d2fd7b12b68c24f7ada7efb3c0e6510faccade
Doug W. <doug.w@pervices.com>
2025-03-04 16:38:05 +0000

    Revert "DEBUG: remove include"
    This reverts commit 594c304624d99e5be0529ea99dc198fdc375bbda.

--- host/lib/usrp/crimson_tng/crimson_tng_impl.cpp
+++ host/lib/usrp/crimson_tng/crimson_tng_impl.cpp
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+#include <boost/assign.hpp>
 #include <boost/asio.hpp>
 #include <functional>
 #include <boost/foreach.hpp>
