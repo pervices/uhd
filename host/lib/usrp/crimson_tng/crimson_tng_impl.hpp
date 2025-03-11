2104959048de10e208336821aaf959cd63a63280
Doug W. <doug.w@pervices.com>
2025-03-11 22:40:41 +0000

    PV_USRP: removing unused variable

--- host/lib/usrp/crimson_tng/crimson_tng_impl.hpp
+++ host/lib/usrp/crimson_tng/crimson_tng_impl.hpp
@@ -205,8 +205,6 @@ private:
         // TODO: see if removing rx_streamers and tx_streamers is viable
         std::vector<std::weak_ptr<uhd::usrp::crimson_tng_recv_packet_streamer>> rx_streamers;
         std::vector<std::weak_ptr<uhd::usrp::crimson_tng_send_packet_streamer>> tx_streamers;
-        std::vector<uhd::transport::zero_copy_if::sptr> rx_dsp_xports;
-        std::vector<uhd::transport::zero_copy_if::sptr> tx_dsp_xports;
         std::vector<uhd::transport::udp_simple::sptr> fifo_ctrl_xports;
     };
     mb_container_type _mbc;
