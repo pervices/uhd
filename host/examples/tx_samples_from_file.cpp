6e1d1b57ac5732458a3cbd1c0b2982d8b7339c91
Doug W. <doug.w@pervices.com>
2025-04-29 19:34:42 +0000

    debug msg

--- host/examples/tx_samples_from_file.cpp
+++ host/examples/tx_samples_from_file.cpp
@@ -218,6 +218,8 @@ void send_from_file(
         size_t samples_to_send;
         samp_type* sample_buffer = sample_manager->get_samples(&samples_to_send);
 
+        printf("sample_buffer: %lu\n", sample_buffer);
+
         std::vector<samp_type*> send_buff_ptr(tx_stream->get_num_channels(), sample_buffer);
 
         const size_t samples_sent = tx_stream->send(send_buff_ptr, samples_to_send, md, 30);
