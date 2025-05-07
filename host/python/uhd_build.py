93ff5d2203956d994d9be65f84e34010c0af583e
Doug W. <doug.w@pervices.com>
2025-05-07 18:36:54 +0000

    DEBUG: disabling function meant to force pietry to make a platform specific wheel

--- host/python/uhd_build.py
+++ host/python/uhd_build.py
@@ -5,9 +5,9 @@ Copyright 2017-2018 Ettus Research, a National Instruments Company
 SPDX-License-Identifier: GPL-3.0-or-later
 """
 
-def build(setup_kwargs):
-    """The build function is a noop. 
-    
-    The empty function forces poetry to build a platform specific wheel
-    """
-    pass
+# def build(setup_kwargs):
+#     """The build function is a noop.
+#
+#     The empty function forces poetry to build a platform specific wheel
+#     """
+#     pass
