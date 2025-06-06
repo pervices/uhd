2870b4cdcb4036c81a727dda93e4f38da68811f1
Doug W. <doug.w@pervices.com>
2025-06-06 15:20:51 +0000

    add IORING_SETUP_DEFER_TASKRUN

--- host/lib/transport/io_uring_recv_manager.cpp
+++ host/lib/transport/io_uring_recv_manager.cpp
@@ -73,7 +73,7 @@ void io_uring_recv_manager::uring_init(size_t ch) {
     // IORING_SETUP_CQSIZE: pay attention to cq_entries
     // TODO: confirm if IORING_SETUP_NO_SQARRAY helps
     // IORING_SETUP_NO_SQARRAY: for some reason this fixes dropped packets when streaming multiple channels
-    uring_params.flags = /*IORING_SETUP_SQ_AFF | */ /*IORING_SETUP_SQPOLL |*/ IORING_SETUP_SINGLE_ISSUER | IORING_SETUP_CQSIZE | IORING_SETUP_COOP_TASKRUN | IORING_SETUP_TASKRUN_FLAG;
+    uring_params.flags = /*IORING_SETUP_SQ_AFF | */ /*IORING_SETUP_SQPOLL |*/ IORING_SETUP_SINGLE_ISSUER | IORING_SETUP_CQSIZE | IORING_SETUP_COOP_TASKRUN | IORING_SETUP_TASKRUN_FLAG | IORING_SETUP_DEFER_TASKRUN;
     // Select the core to bind the kernel thread to
     // Does nothing unless flag IORING_SETUP_SQ_AFF is set.
     //uring_params.sq_thread_cpu;
