a11bd0db9b2c650dac0622a06d8b8ebc67b5ca4a
Doug W. <doug.w@pervices.com>
2025-06-06 16:39:25 +0000

    Reducing timeout time

--- host/include/uhd/transport/io_uring_recv_manager.hpp
+++ host/include/uhd/transport/io_uring_recv_manager.hpp
@@ -124,8 +124,8 @@ private:
         // Get new completion events
         struct __kernel_timespec time_limit;
         memset(&time_limit, 0, sizeof(time_limit));
-        time_limit.tv_sec = 1;
-        time_limit.tv_nsec = 0;
+        time_limit.tv_sec = 0;
+        time_limit.tv_nsec = 1000;
         int r = io_uring_wait_cqes(ring, completion_cache[ch], COMPLETION_EVENT_CACHE_SIZE, &time_limit, 0);
 
         // If events ready
