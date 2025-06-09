fe744a1f2e8a49b94748b91d2adef98f99ffce20
Doug W. <doug.w@pervices.com>
2025-06-09 20:54:41 +0000

    DEBUG: increasing usleep to 10

--- host/include/uhd/transport/io_uring_recv_manager.hpp
+++ host/include/uhd/transport/io_uring_recv_manager.hpp
@@ -114,7 +114,7 @@ private:
             *cqe_ptr = completion_cache[ch][cached_cqe_consumed[ch]];
             return 0;
         }
-        ::usleep(1);
+        ::usleep(10);
 
         // Marks all events in the cache as completed
         io_uring* ring = access_io_urings(ch);
