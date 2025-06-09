f7d2b89f0030c8d85d05565d26c5c14c105a30f9
Doug W. <doug.w@pervices.com>
2025-06-09 21:19:05 +0000

    DEBUG: issuing io_uring start commands in their own thread

--- host/include/uhd/transport/io_uring_recv_manager.hpp
+++ host/include/uhd/transport/io_uring_recv_manager.hpp
@@ -158,7 +158,7 @@ private:
      * Arms recv_multishot
      * Must be called after flushing the sockets and after any time the completion queue gets full
      */
-    void arm_recv_multishot(size_t ch, int fd);
+    static void arm_recv_multishot(io_uring_recv_manager* self, size_t ch, int fd);
 
     /**
      * Number of completion events in the cache
