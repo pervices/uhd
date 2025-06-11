f2c84905c690d532521d3286dc12c853dcbd9716
Doug W. <doug.w@pervices.com>
2025-06-11 20:05:23 +0000

    IO_URING: restart recv multishot if the buffer is full
    Previously it would stop when the buffer filled up

--- host/include/uhd/transport/io_uring_recv_manager.hpp
+++ host/include/uhd/transport/io_uring_recv_manager.hpp
@@ -26,7 +26,9 @@ private:
     // Number of entries in each uring
     // Should be a power of 2 to avoid confusion since most kernels round this up to the next power of 2
     // Hard limit: 2 ^ 15
-    static constexpr uint32_t NUM_SQ_URING_ENTRIES = 1;
+    // Ideally the submission queue only needs to fit 1 multishot command, but rearming it may require more space due to race conditions
+    // TODO: reduce the submission queue size once rearming always works. Currently it doesn't always rearm when stopping
+    static constexpr uint32_t NUM_SQ_URING_ENTRIES = 32768;
     static constexpr uint32_t NUM_CQ_URING_ENTRIES = PACKET_BUFFER_SIZE;
     // These constants must be a power of 2. Documentation says most kernels will automatically rounds up to the next power of 2, experimentally not having them as a power of 2 causes some liburing functions to fail
     static_assert((NUM_SQ_URING_ENTRIES>0 && ((NUM_SQ_URING_ENTRIES & (NUM_SQ_URING_ENTRIES-1)) == 0)), "NUM_SQ_URING_ENTRIES must be a power of 2");
