a85282245fc70f5addf724910a09ee50b43c8a91
Doug W. <doug.w@pervices.com>
2025-06-20 12:43:03 +0000

    Prefer TX LO below requested frequency and RX above

--- host/lib/usrp/cyan_nrnt/cyan_nrnt_impl.hpp
+++ host/lib/usrp/cyan_nrnt/cyan_nrnt_impl.hpp
@@ -245,7 +245,7 @@ private:
     void program_stream_dest(uhd::transport::zero_copy_if::sptr &, const uhd::stream_args_t &);
 
     // Calculate and set frequency
-    double choose_lo_shift( double target_freq, int band, property_tree::sptr dsp_subtree, int xx_sign, size_t chan );
+    double choose_lo_shift( double target_freq, int band, property_tree::sptr dsp_subtree, bool is_tx, size_t chan );
     tune_result_t tune_xx_subdev_and_dsp( const double xx_sign, property_tree::sptr dsp_subtree, property_tree::sptr rf_fe_subtree, const tune_request_t &tune_request, int* gain_is_set, int* last_set_band, size_t chan );
 
     uhd::tune_result_t set_rx_freq(const uhd::tune_request_t &tune_request, size_t chan = 0);
