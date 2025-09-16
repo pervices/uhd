75a701dbe1df0d0e167ad6953bcf831192826e28
Chen Jin <chen.j@pervices.com>
2025-09-16 15:13:48 -0400

    DO NOT MERGE: substract all subsequent timestamps by 5000

--- host/lib/include/uhdlib/transport/super_send_packet_handler_mmsg.hpp
+++ host/lib/include/uhdlib/transport/super_send_packet_handler_mmsg.hpp
@@ -318,6 +318,8 @@ private:
     // Gets the number of samples that can be sent now (can be less than 0)
     int check_fc_npackets(const size_t ch_i);
 
+    bool is_first_packet = true;
+
     UHD_INLINE size_t send_multiple_packets(
         const uhd::tx_streamer::buffs_type &sample_buffs,
         const size_t nsamps_to_send,
@@ -353,13 +355,33 @@ private:
             packet_header_infos[n].has_tlr = false; // No trailer
             packet_header_infos[n].has_tsi = false; // No integer timestamp
             packet_header_infos[n].has_tsf = true; // Always include a fractional timestamp (in ticks of _TICK_RATE)
-            if(metadata_.has_time_spec) {
-                // Sets the timestamp based on what's specified by the user
-                packet_header_infos[n].tsf = (metadata_.time_spec + time_spec_t::from_ticks(n * _max_samples_per_packet - nsamps_in_cache, _sample_rate)).to_ticks(_TICK_RATE);
+            // if(metadata_.has_time_spec) {
+            //     // Sets the timestamp based on what's specified by the user
+            //     packet_header_infos[n].tsf = (metadata_.time_spec + time_spec_t::from_ticks(n * _max_samples_per_packet - nsamps_in_cache, _sample_rate)).to_ticks(_TICK_RATE);
+            // } else {
+            //     // Sets the timestamp to follow from the previous send
+            //     packet_header_infos[n].tsf = (next_send_time + time_spec_t::from_ticks(n * _max_samples_per_packet - nsamps_in_cache, _sample_rate)).to_ticks(_TICK_RATE);
+            // }
+
+            if(is_first_packet) {
+                if(metadata_.has_time_spec) {
+                    // Sets the timestamp based on what's specified by the user
+                    packet_header_infos[n].tsf = (metadata_.time_spec + time_spec_t::from_ticks(n * _max_samples_per_packet, _sample_rate)).to_ticks(_TICK_RATE);
+                } else {
+                    // Sets the timestamp to follow from the previous send
+                    packet_header_infos[n].tsf = (next_send_time + time_spec_t::from_ticks(n * _max_samples_per_packet, _sample_rate)).to_ticks(_TICK_RATE);
+                }
             } else {
-                // Sets the timestamp to follow from the previous send
-                packet_header_infos[n].tsf = (next_send_time + time_spec_t::from_ticks(n * _max_samples_per_packet - nsamps_in_cache, _sample_rate)).to_ticks(_TICK_RATE);
+                if(metadata_.has_time_spec) {
+                    // Sets the timestamp based on what's specified by the user
+                    packet_header_infos[n].tsf = (metadata_.time_spec + time_spec_t::from_ticks(n * _max_samples_per_packet, _sample_rate)).to_ticks(_TICK_RATE) - 5000;
+                } else {
+                    // Sets the timestamp to follow from the previous send
+                    packet_header_infos[n].tsf = (next_send_time + time_spec_t::from_ticks(n * _max_samples_per_packet, _sample_rate)).to_ticks(_TICK_RATE) - 5000;
+                }
             }
+            is_first_packet = false;
+
             packet_header_infos[n].sob = (n == 0) && metadata_.start_of_burst;
             packet_header_infos[n].eob     = metadata_.end_of_burst;
             packet_header_infos[n].fc_ack  = false; // Is not a flow control packet
