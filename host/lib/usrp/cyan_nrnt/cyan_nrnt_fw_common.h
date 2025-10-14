a93767d8637f6d0d75015005f9954d8f6492446f
Chen Jin <chen.j@pervices.com>
2025-10-14 17:49:52 -0400

    Increased threshold to 70%

--- host/lib/usrp/cyan_nrnt/cyan_nrnt_fw_common.h
+++ host/lib/usrp/cyan_nrnt/cyan_nrnt_fw_common.h
@@ -56,7 +56,7 @@
 #define CYAN_NRNT_UPDATE_PER_SEC	100
 
 //how full UHD should try to keep the buffer
-#define CYAN_NRNT_BUFF_PERCENT 0.5
+#define CYAN_NRNT_BUFF_PERCENT 0.4
 
 // Cyan NrNt tx RF gain limits, this is definitely wrong
 // TODO: get this info from device
