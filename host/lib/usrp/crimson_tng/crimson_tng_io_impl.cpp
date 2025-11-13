62e1e41c643823235efbcf3271faecf1bc39c93e
Joseph Talon <joseph.t@pervices.com>
2025-11-13 21:28:52 +0000

    Added debug print statement

--- host/lib/usrp/crimson_tng/crimson_tng_io_impl.cpp
+++ host/lib/usrp/crimson_tng/crimson_tng_io_impl.cpp
@@ -288,7 +288,7 @@ void crimson_tng_send_packet_streamer::check_tx_rates() {
     std::vector<eprops_type> local_eprops = std::vector<eprops_type>(_eprops);
     // local_eprops.assign(_eprops.begin(), _eprops.end());
     
-    for (auto e : local_eprops) {
+    for (auto &e : local_eprops) {
         std::cout << e.sample_rate << std::endl;
         std::cout << e.name << std::endl;
     }
