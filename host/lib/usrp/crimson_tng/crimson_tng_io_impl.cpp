c969c8ac256b9421b29c486c35bd3af0bd8cabf8
Joseph Talon <joseph.t@pervices.com>
2025-11-13 19:35:31 +0000

    Added debug print statement

--- host/lib/usrp/crimson_tng/crimson_tng_io_impl.cpp
+++ host/lib/usrp/crimson_tng/crimson_tng_io_impl.cpp
@@ -295,6 +295,7 @@ void crimson_tng_send_packet_streamer::check_tx_rates() {
         i++;
         return ep;
     });
+    std::cout << "EPROPS 0: " << _eprops[0].name << std::endl;
 
     // Sort the vector in ascending order of sample rates
     std::sort(local_eprops.begin(), local_eprops.end(), [](eprops_type a, eprops_type b) {
