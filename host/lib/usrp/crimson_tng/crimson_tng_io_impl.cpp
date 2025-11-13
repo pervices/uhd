5bf724371707e8bfb2852c9338599292ef4b0980
Joseph Talon <joseph.t@pervices.com>
2025-11-13 22:53:12 +0000

    Added debug print statement

--- host/lib/usrp/crimson_tng/crimson_tng_io_impl.cpp
+++ host/lib/usrp/crimson_tng/crimson_tng_io_impl.cpp
@@ -298,7 +298,7 @@ void crimson_tng_send_packet_streamer::check_tx_rates() {
     std::cout << "local eprops 0 name: " << local_eprops[0].name << std::endl;
 
     // Sort the vector in ascending order of sample rates
-    std::sort(local_eprops.begin(), local_eprops.end(), [](eprops_type a, eprops_type b) {
+    std::stable_sort(local_eprops.begin(), local_eprops.end(), [](eprops_type &a, eprops_type &b) {
         UHD_LOG_INFO(CRIMSON_TNG_DEBUG_NAME_C, "Comparing sample rates of " + a.name + " and " + b.name);
                 UHD_LOG_INFO(CRIMSON_TNG_DEBUG_NAME_C, "Comparing sample rates of " + std::to_string(a.sample_rate) + " and " + std::to_string(b.sample_rate));
         return a.sample_rate < b.sample_rate;
