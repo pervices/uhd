031bc4771b06c11147c50f83927e35f99901f2e5
Doug W. <doug.w@pervices.com>
2025-12-11 21:10:27 +0000

    fixup

--- host/lib/utils/pv_tx_async_msg_queue.cpp
+++ host/lib/utils/pv_tx_async_msg_queue.cpp
@@ -6,8 +6,10 @@
 
 #include <uhdlib/utils/pv_tx_async_msg_queue.hpp>
 
-pv_tx_async_msg_queue::pv_tx_async_msg_queue(size_t num_channels, size_t max_messages_per_channel = 1000) {
+namespace uhd {
+    pv_tx_async_msg_queue::pv_tx_async_msg_queue(size_t num_channels, size_t max_messages_per_channel) {
 
-    // The message vector of size num_channels by max_messages_per_channel
-    messages = std::vector<std::vector<checksum_msg>>(num_channels, std::vector<checksum_msg>(max_messages_per_channel));
+        // The message vector of size num_channels by max_messages_per_channel
+        messages = std::vector<std::vector<checksum_msg>>(num_channels, std::vector<checksum_msg>(max_messages_per_channel));
+    }
 }
