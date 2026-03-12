3b1cba008b6afd5a54877fd21a4eadf1158caa08
Joseph Talon <joseph.t@pervices.com>
2026-03-12 16:47:50 +0000

    CYAN: Unlock correct sfp control mutex in bm_thread_fn

--- host/lib/usrp/cyan_nrnt/cyan_nrnt_impl.cpp
+++ host/lib/usrp/cyan_nrnt/cyan_nrnt_impl.cpp
@@ -703,7 +703,7 @@ void cyan_nrnt_impl::bm_thread_fn( cyan_nrnt_impl *dev ) {
 
         // Unlock sfp control mutex here
         // It is no longer needed, and having it will deadlock if time_diff_process triggers a reset
-        dev->_sfp_control_mutex[0]->unlock();
+        dev->_sfp_control_mutex[dev->get_which_time_diff_iface()]->unlock();
         if (reply_good) {
             dev->time_diff_process( tdr, now );
         } else if (!dropped_recv_message_printed && dev->clock_sync_desired) {
