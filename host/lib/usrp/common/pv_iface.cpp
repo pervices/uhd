de9b8dc87958f21c86d295669bc568306fc876c1
Doug W. <doug.w@pervices.com>
2026-04-10 12:44:39 +0000

    debug msg

--- host/lib/usrp/common/pv_iface.cpp
+++ host/lib/usrp/common/pv_iface.cpp
@@ -112,6 +112,7 @@ std::string pv_iface::peek_str( float timeout_s ) {
             nbytes = tcp_connection->recv(_buff, MAX_MTU_SIZE, timeout_s);
         }
 
+        printf("peek_str nbytes: %lu\n", nbytes);
         std::cout << "peek_str _buff: ";
         std::cout.write(_buff, nbytes);
         std::cout << "\n";
