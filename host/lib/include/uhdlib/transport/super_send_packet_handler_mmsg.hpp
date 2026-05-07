b0329760a377d25d58997336a641a6defff973fc
Doug W. <doug.w@pervices.com>
2026-05-07 16:54:08 +0000

    more removing sources of floating point rounding error

--- host/lib/include/uhdlib/transport/super_send_packet_handler_mmsg.hpp
+++ host/lib/include/uhdlib/transport/super_send_packet_handler_mmsg.hpp
@@ -261,7 +261,7 @@ private:
 
     // The start time of the next batch of samples in ticks
     // The FPGA requires a timestampt always be present in packets. This is used to figureout the timestamp when not specified by the user
-    uhd::time_spec_t next_send_time = uhd::time_spec_t(0.0);
+    int64_t next_send_time = 0;
 
     //TODO move all the vectors with channel specific info here
     // Stores information about packets to send for each channel
@@ -386,7 +386,7 @@ private:
                 packet_header_infos[n].tsf = metadata_.time_spec.to_ticks(_TICK_RATE) + samples_to_ticks(n * _max_samples_per_packet - nsamps_in_cache);
             } else {
                 // Sets the timestamp to follow from the previous send
-                packet_header_infos[n].tsf = next_send_time.to_ticks(_TICK_RATE) + samples_to_ticks(n * _max_samples_per_packet - nsamps_in_cache);
+                packet_header_infos[n].tsf = next_send_time + samples_to_ticks(n * _max_samples_per_packet - nsamps_in_cache);
             }
             packet_header_infos[n].sob = (n == 0) && metadata_.start_of_burst;
             packet_header_infos[n].eob     = metadata_.end_of_burst;
@@ -604,10 +604,11 @@ private:
         );
 
         // Updates the next timestamp to follow from the end of this send
+        // TODO: verify this is correct, it may be the source of discontinuities
         if(metadata_.has_time_spec) {
-            next_send_time = metadata_.time_spec + time_spec_t::from_ticks(samples_sent, _sample_rate);
+            next_send_time = metadata_.time_spec.to_ticks(_TICK_RATE) + samples_to_ticks(samples_sent);
         } else {
-            next_send_time = next_send_time + time_spec_t::from_ticks(samples_sent, _sample_rate);
+            next_send_time = next_send_time + samples_to_ticks(samples_sent);
         }
 
         // Increment the sequence number counter by the number of packets actually sent
