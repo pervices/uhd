1d7e3f3d39b88fbaa3d89691c8296a485672a64a
Doug Wilson <doug.w@pervices.com>
2026-05-28 11:38:36 -0400

    WIP: moving clock sync operations to it's class
    Preparation for more funamental sync changes

--- host/lib/include/uhdlib/usrp/common/clock_sync.hpp
+++ host/lib/include/uhdlib/usrp/common/clock_sync.hpp
@@ -15,10 +15,35 @@
 #include <memory>
 // Fences
 #include <immintrin.h>
+// Socket for sending/receiving time diff packets
+#include <uhd/transport/udp_simple.hpp>
+// Standard library for thread
+#include <thread>
+// PID controller for clock sync
+#include "pidc.hpp"
+// Data type used within UHD for storing time
+#include <uhd/types/time_spec.hpp>
 
 namespace uhd { namespace usrp {
 
 static constexpr size_t CACHE_LINE_SIZE = 64;
+
+#pragma pack(push,1)
+// Time diff requests to the device
+struct time_diff_req {
+    uint64_t header;
+    int64_t tv_sec;
+    int64_t tv_tick;
+};
+#pragma pack(pop)
+
+#pragma pack(push,1)
+// Time diff replies from the device
+struct time_diff_resp {
+    int64_t tv_sec;
+    int64_t tv_tick;
+};
+#pragma pack(pop)
 
 // Stores data shared between the clock sync thread and any other thread that requires the time
 // Intened use:
@@ -27,6 +52,10 @@ static constexpr size_t CACHE_LINE_SIZE = 64;
 class clock_sync_shared_info
 {
 private:
+
+    static constexpr double UPDATES_PER_SECOND = 100;
+    // TODO: make UPDATE_PERIOD a constexpr (will require changes to the class)
+    static inline uhd::time_spec_t UPDATE_PERIOD = uhd::time_spec_t(1.0/UPDATES_PER_SECOND);
 
     // Stores if the predicted time and actual time have convered (clock sync completed)
     alignas(CACHE_LINE_SIZE) bool is_converged = false;
@@ -37,11 +66,41 @@ private:
     // TODO: verify alignas is working properly
     alignas(CACHE_LINE_SIZE) double time_diff = 0;
 
-    // Declare constructor as private to ensure this is only created through make
-    clock_sync_shared_info() {
-        // Move this to a .cpp file if it becomes complicated
-        /* No-op*/
-    }
+    // TODO: ensure all variable after this line are aligned to avoid false sharing
+
+    // Tells the sync thread to exit
+    bool sync_thread_should_exit = false;
+
+    // The socket used to send and receive time diffs
+    transport::udp_simple::sptr sync_socket;
+
+    // Tick rate of time diff packets
+    const double _tick_rate;
+
+    // TODO: have the device set this to true when we know it is needed
+    bool clock_sync_desired = true;
+
+    std::thread sync_thread;
+
+    /** PID controller that rejects differences between Crimson's clock and the host's clock.
+     *  -> The Set Point of the controller (the desired input) is the desired error between the clocks - zero!
+     *  -> The Process Variable (the measured value), is error between the clocks, as computed by Crimson.
+     *  -> The Control Variable of the controller (the output) is the required compensation for the host
+     *     such that the error is forced to zero.
+     *     => Crimson Time Now := Host Time Now + CV
+     */
+    uhd::pidc time_diff_pidc;
+
+    /**
+     * Create an instance of clock_sync_shared_info.
+     * The constructor is private to ensure this is only created through make.
+     * @param ip The ip address to perform clock sync with
+     * @param port The port the device receives clock sync messages on
+     * @param tick_rate The tick rate the device expects to time diff packets
+     */
+    clock_sync_shared_info(std::string ip, uint16_t port, double tick_rate);
+
+    ~clock_sync_shared_info();
 
     // Deleter to be used by a shared_ptr
     struct deleter {
@@ -54,6 +113,48 @@ private:
             free(self);
         }
     };
+
+    /**
+     * Send a packet with the predicted time.
+     * Send is inline but not recv because send may be sent from a critical thread, recv is not time sensitive
+     * @param prediction The predicted time on the device
+     */
+    inline void time_diff_send( uhd::time_spec_t prediction ) {
+        time_diff_req request;
+
+        // Create request
+        request.header = (uint64_t)0x20002 << 16;
+        request.tv_sec = prediction.get_full_secs();
+        request.tv_tick = (int64_t) ( prediction.get_frac_secs() * 1e9 / _tick_rate );
+
+        // Convert request from native little endian to big endian for the FPGA
+        // TODO: detect if we are using big or little endian at compile time
+        request.header = __builtin_bswap64(request.header);
+        request.tv_sec = __builtin_bswap64(request.tv_sec);
+        request.tv_tick = __builtin_bswap64(request.tv_tick);
+
+        sync_socket->send(&request, sizeof(request));
+    }
+
+    /**
+     * Receives a packet a packet containing prediction - actual
+     * @param reply Where to store hte reply
+     * @return Returns true on success, false on failure
+     */
+    bool time_diff_recv(time_diff_resp & reply);
+
+    /**
+     * Resets the time diff pid.
+     * This function will reset the time diff pid and create a fresh prediction
+     */
+    void reset_time_diff_pid();
+
+    /**
+     * Updates the pid controller for predicting the time
+     * @param tdr The reply containing the difference between the predicted and actual time
+     * @param now The time on the host when the request was send
+     */
+    void time_diff_process( const time_diff_resp & tdr, const uhd::time_spec_t & request_time );
 
 public:
     /**
@@ -118,10 +219,26 @@ public:
     }
 
     /**
+     * Sets whether or not clock sync is needed.
+     * When clock sync is not needed error messages are supressed an
+     * @param desired True indicates clock sync is needed
+     */
+    void set_clock_sync_desired(bool desired);
+
+    /**
      * Create a new clock_sync_shared_info with an associated shared pointer on it's own cache line
+     * @param ip The ip address to perform clock sync with
+     * @param port The port the device receives clock sync messages on
+     * @param tick_rate The tick rate the device expects to time diff packets
      * @return Shared pointer to the newly created instance
      */
-    static std::shared_ptr<clock_sync_shared_info> make();
+    static std::shared_ptr<clock_sync_shared_info> make(std::string ip, uint16_t port, double tick_rate);
+
+    /**
+     * The loop to run clock sync in
+     * @param self The class to run clock sync for. A raw pointer is used because the deleter is responsible for ending the thread.
+     */
+    static void loop_thread_fn(clock_sync_shared_info *self);
 
 };
 
