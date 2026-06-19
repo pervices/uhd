6f7f53c901e2ce2efcacee710ed0b6274e775a71
Doug Wilson <doug.w@pervices.com>
2026-06-19 12:37:31 -0400

    WIP: TIME_SPEC_HP: creating high performance integer only timespec_t
    The aim of this class is to avoid floating point rounding error while
    still being atomic.

---
+++ host/lib/include/uhdlib/types/time_spec_hp.hpp
@@ -0,0 +1,20 @@
+//
+// Copyright 2026 Per Vices Corporation
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+namespace uhd::usrp {
+    /**
+     * A high performance integer only timespec that supports atomic operations
+     */
+    class alignas(16) time_spec_hp{
+        // The seconds portion of time
+        int64_t seconds;
+        // The tick portion of time
+        int64_t ticks;
+        // The tick rate in Hz
+        double tick_rate;
+
+    };
+}
