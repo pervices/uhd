8ccd82f56f3331cd8dbcfc1cbed27c62aa652fdf
Doug Wilson <doug.w@pervices.com>
2026-06-22 11:38:47 -0400

    TIME_SPEC_HP: initial commit of an atomic timespec

---
+++ host/lib/include/uhdlib/types/time_spec_atomic.hpp
@@ -0,0 +1,7 @@
+//
+// Copyright 2026 Per Vices Corporation
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+#pragma once
---
+++ host/lib/types/time_spec_atomic.cpp
@@ -0,0 +1,5 @@
+//
+// Copyright 2026 Per Vices Corporation
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
