cb282d4fe75bc52928669d7be560a017ffc13772
Doug Wilson <doug.w@pervices.com>
2026-06-22 17:20:19 -0400

    Moving template functions to header

--- host/lib/include/uhdlib/utils/swmr.hpp
+++ host/lib/include/uhdlib/utils/swmr.hpp
@@ -35,59 +35,84 @@ namespace uhd {
 
     public:
 
-        // --- 1. LIFE-CYCLE CONSTRUCTORS ---
-
         /**
          * The default initialize to 0
          */
-        explicit swmr() noexcept;
+        explicit swmr() noexcept {
+            write_count.store(0, std::memory_order_release);
+        }
 
         /**
          * Initialize to store existing data
          */
-        explicit swmr(T data) noexcept;
+        explicit swmr(T data) noexcept {
+            // We can do a relaxed store here because we do a release during store
+            write_count.store(0, std::memory_order_relaxed);
+            store(data);
+        }
 
         /**
          * NOTE: this is thread safe against loads, not against multiple writers
          */
-        void store(T dst);
+        inline void store(T data) {
+            // Increment write counter to ensure the reader thread knows a write has started
+            write_count.fetch_add(1, std::memory_order_relaxed);
 
-        void load(T* dst);
+            /**
+             * Release fence to ensure write_count has been updated
+             * A fence is required to prevent writes to _data from being reordered earlier,
+             * which making the fetch_add memory_order_release alone would not do
+             */
+            std::atomic_thread_fence(std::memory_order_release);
 
-        // --- 2. DELETED STRUCTURAL COPY/MOVE ---
-        // Moving or copying multi-member structures results in data tearing
+            _data = data;
+
+            // Fence to ensure _data is done being written to before write_count is updated
+            std::atomic_thread_fence(std::memory_order_release);
+
+            // Increment the write counter so the consumer knows we are done writting
+            write_count.fetch_add(1, std::memory_order_release);
+        }
+
+        inline void load(T* dst) {
+            int64_t intial_write_count = 0;
+            T loaded_data;
+            int64_t end_write_count = 0;
+
+            do {
+                // Get the value of write_count at the start of copying
+                intial_write_count = write_count.load(std::memory_order_relaxed);
+
+                // Fence to ensure reads of _data do not get reordered before reads for intial_write_count
+                std::atomic_thread_fence(std::memory_order_acquire);
+
+                memcpy(dst, &_data, sizeof(T));
+
+                // Fence to ensure the read for end_write_count does not get reordered before copying _data
+                std::atomic_thread_fence(std::memory_order_acquire);
+
+                end_write_count= write_count.load(std::memory_order_relaxed);
+
+            } while (
+                // Repeat load if the class was updated while loading
+                intial_write_count != end_write_count ||
+                // Repeat load if the class is mid update (write_count is odd)
+                (intial_write_count & 0x1) );
+
+        }
+
+        /**
+         * Delete copy and move operators to avoid accidental non thread safe copies and moves
+         */
+
+        // Delete copy constructor
         swmr(const swmr&) = delete;
-        swmr(swmr&&) = delete;
-
-        // --- 3. DELETED COMPILER-GENERATED DEFAULT ASSIGNMENTS & CASTS ---
-        // Prevents direct raw object assignments or dangerous structural downgrades
+        // Delete copy assignment
         swmr& operator=(const swmr&) = delete;
-
-        // --- 4. DELETED MEMORY POINTER ALIASING ---
-        // Direct addresses let threads modify individual fields bypassing internal sync
-        swmr* operator&() = delete;
-        const swmr* operator&() const = delete;
-        swmr* operator->() = delete;
-        swmr& operator*() = delete;
-
-        // --- 5. DELETED READ-MODIFY-WRITE (RMW) OPERATORS ---
-        // Math operations on a multi-type structure are inherently unaligned and unsafe
-        swmr operator++(int) = delete;
-        swmr operator--(int) = delete;
-        swmr& operator++()    = delete;
-        swmr& operator--()    = delete;
-
-        swmr& operator+=(const swmr&) = delete;
-        swmr& operator-=(const swmr&) = delete;
-
-        // --- 6. DELETED CONDITIONAL EVALUATIONS ---
-        // Block multi-step branches which cause race conditions
-        bool operator==(const swmr&) const  = delete;
-        bool operator!=(const swmr&) const  = delete;
-        bool operator<(const swmr&) const   = delete;
-        bool operator<=(const swmr&) const  = delete;
-        bool operator>(const swmr&) const   = delete;
-        bool operator>=(const swmr&) const  = delete;
+        // Delete move constructor
+        swmr(swmr&&) = delete;
+        // Delete move assignment
+        swmr& operator=(swmr&&) = delete;
     };
 
 } // namepspace uhd
