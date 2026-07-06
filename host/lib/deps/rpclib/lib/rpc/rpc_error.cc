dbb8c95b0f3886d6a6ddc3178275896d517f77fa
Doug Wilson <doug.w@pervices.com>
2026-07-06 10:31:36 -0400

    next file

--- host/lib/deps/rpclib/lib/rpc/rpc_error.cc
+++ host/lib/deps/rpclib/lib/rpc/rpc_error.cc
@@ -1,5 +1,5 @@
 #include "rpc/rpc_error.h"
-#include <boost/format.hpp>
+#include <format>
 
 namespace rpc {
 
@@ -15,8 +15,7 @@ std::string rpc_error::get_function_name() const { return func_name_; }
 RPCLIB_MSGPACK::object_handle &rpc_error::get_error() { return ob_h_; }
 
 timeout::timeout(std::string const &what_arg) : std::runtime_error(what_arg) {
-    formatted =
-        str(boost::format("rpc::timeout: %s") % std::runtime_error::what());
+    formatted = std::format("rpc::timeout: {}", this->what());
 }
 
 const char *timeout::what() const noexcept { return formatted.data(); }
