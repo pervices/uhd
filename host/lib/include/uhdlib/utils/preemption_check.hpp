c7b3bdfafe4249f8cc89606a6c6c12c09dd3969b
Doug Wilson <doug.w@pervices.com>
2026-07-15 12:27:04 -0400

    PREEMPTION_CHECK: add helper function to check preemption mode
    Preemption can cause random slowdowns. This add a helper function to
    check it. Currently the check is only performed by a function within rx.

---
+++ host/lib/include/uhdlib/utils/preemption_check.hpp
@@ -0,0 +1,29 @@
+//
+// Copyright 2026 Per Vices Corporation
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+#pragma once
+
+#include <string>
+
+namespace uhd {
+
+    // Copy of the enum from kernel/sched/core.c that defines preemption types
+    typedef enum {
+        PREEMPT_DYNAMIC_UNDEFINED = -1,
+        PREEMPT_DYNAMIC_NONE      = 0,
+        PREEMPT_DYNAMIC_VOLUNTARY = 1,
+        PREEMPT_DYNAMIC_FULL      = 2,
+        PREEMPT_DYNAMIC_LAZY      = 3
+    } preempt_mode_t;
+
+    /**
+     * Check if preemption is enabled and warn the user if it isn't
+     * @param log_header The header to use during the print message
+     * @return Returns the preemption mode on success. Returns PREEMPT_DYNAMIC_UNDEFINED on failure
+     */
+    preempt_mode_t check_preemption(std::string log_header = "PREEMPTION_CHECK");
+
+}; /* namespace uhd */
