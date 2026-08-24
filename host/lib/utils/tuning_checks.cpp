9ab501cbc25c933a40c122bf498f3029366efd0b
Doug Wilson <doug.w@pervices.com>
2026-08-24 17:48:28 -0400

    CHECK_NUMA: adding stub to check NUMA node for sockets

---
+++ host/lib/utils/tuning_checks.cpp
@@ -0,0 +1,11 @@
+//
+// Copyright 2026 Per Vices Corporation
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+#include <uhdlib/utils/tuning_checks.hpp>
+
+int uhd::check_numa(const cpu_set_t affinity_mask, int socket_fd[], size_t socket_fd_len) {
+    
+}
