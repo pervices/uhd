8cd6ad7065e2f2441d620d22302d0b821877e6b0
Doug Wilson <doug.w@pervices.com>
2026-08-24 17:48:28 -0400

    CHECK_NUMA: adding stub to check NUMA node for sockets

---
+++ host/lib/include/uhdlib/utils/tuning_checks.hpp
@@ -0,0 +1,30 @@
+//
+// Copyright 2026 Per Vices Corporation
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+#pragma once
+
+#include <cstddef>
+#include <sched.h>
+
+namespace uhd {
+
+// A collection of functions for checking if the program is tuned properly
+
+/**
+ * Check if the provided affinity mask will keep the network sockets on the correct NUMA node.
+ * Prints a warning if the mask will allow a thread to run on a different NUMA node than the sockets.
+ *
+ * @param affinity_mask The mask indicating which cores the thread being checked can use.
+ * @param socket_fd An array of file descriptors for the sockets to check.
+ * @param socket_fd_len The number of elements in socket_fd.
+ *
+ * @throws system_error Unable to get the NUMA node for the provided sockets.
+ *
+ * @return 0 if affinity_mask will keep a thread on the correct NUMA node for socket_fd, non-zero otherwise.
+ */
+int check_numa(const cpu_set_t affinity_mask, int socket_fd[], size_t socket_fd_len);
+
+}; /* namespace uhd */
