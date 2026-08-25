da062730ced7a80cc4bc106d04c11b0ebd8e4432
Doug Wilson <doug.w@pervices.com>
2026-08-25 17:00:05 -0400

    Implementing getting the network interfaces

--- host/lib/utils/tuning_checks.cpp
+++ host/lib/utils/tuning_checks.cpp
@@ -8,11 +8,17 @@
 
 #include <uhd/exception.hpp>
 #include <uhd/utils/log.hpp>
+#include <uhdlib/utils/network_config.hpp>
 
 #include <numa.h>
 #include <cerrno>
 #include <cstring>
 #include <string>
+#include <vector>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 int uhd::check_numa(const cpu_set_t affinity_mask, int socket_fd[], size_t socket_fd_len) {
 
@@ -60,9 +66,38 @@ int uhd::check_numa(const cpu_set_t affinity_mask, int socket_fd[], size_t socke
         }
     }
 
-    // TODO: get the network interfaces of socket_fd
+    // Get the network interface used by each socket
     std::vector<std::string> network_interfaces;
+    network_interfaces.reserve(socket_fd_len);
 
+    for(size_t n = 0; n < socket_fd_len; n++) {
+        // Gets the local address the socket is bound to (either explicitly via bind(),
+        // or implicitly assigned by the kernel when connect() selected a route)
+        struct sockaddr_in local_addr;
+        socklen_t addr_len = sizeof(local_addr);
+        if(getsockname(socket_fd[n], (struct sockaddr*)&local_addr, &addr_len) < 0) [[unlikely]] {
+            std::string get_sockname_error_message = "Unable to get local address for socket " + std::to_string(socket_fd[n]) + ". getsockname failed with error code: " + std::string(strerror(errno));
+
+            // TODO: add a parameter to take a context specific error message
+            UHD_LOG_WARNING("CHECK_NUMA", get_sockname_error_message);
+            throw uhd::system_error(get_sockname_error_message);
+        }
+
+        char ip_buff[INET_ADDRSTRLEN];
+        if(inet_ntop(AF_INET, &local_addr.sin_addr, ip_buff, INET_ADDRSTRLEN) == nullptr) [[unlikely]] {
+            std::string inet_ntop_error_message = "Unable to convert address for socket " + std::to_string(socket_fd[n]) + " to a string. inet_ntop failed with error code: " + std::string(strerror(errno));
+
+            // TODO: add a parameter to take a context specific error message
+            UHD_LOG_WARNING("CHECK_NUMA", inet_ntop_error_message);
+            throw uhd::system_error(inet_ntop_error_message);
+        }
+
+        // Get the device used by the ip.
+        // get_dev_from_ipv4 will throw errors if it fails
+        std::string interface = uhd::get_dev_from_ipv4(std::string(ip_buff));
+
+        network_interfaces.push_back(interface);
+    }
 
     // TODO: get the numa node of said interfaces
 
