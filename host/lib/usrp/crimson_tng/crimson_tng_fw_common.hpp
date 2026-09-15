02a4a92f16c8cb0cd8d2a4d924f71ae6ac084905
Doug Wilson <doug.w@pervices.com>
2026-09-15 11:59:40 -0400

    CRIMSON: reducing minimum tx start delay
    Now that the start time is calculated after printing we can reduce the
    minimum delay.

--- host/lib/usrp/crimson_tng/crimson_tng_fw_common.hpp
+++ host/lib/usrp/crimson_tng/crimson_tng_fw_common.hpp
@@ -205,4 +205,4 @@ constexpr uint32_t CRIMSON_TNG_VITA_TLR =
 inline const uhd::fs_path CRIMSON_TNG_MB_PATH{"/mboards/0"};
 inline const uhd::fs_path CRIMSON_TNG_TIME_PATH{CRIMSON_TNG_MB_PATH / "time"};
 
-constexpr double CRIMSON_TNG_MIN_TX_DELAY = 0.001;
+constexpr double CRIMSON_TNG_MIN_TX_DELAY = 0.0002;
