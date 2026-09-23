16bbec315e398a11ea9b18d90eb08fc856f76187
Doug Wilson <doug.w@pervices.com>
2026-09-23 17:36:20 -0400

    WIP: modify vita header to ignore trailer

--- host/lib/transport/super_recv_packet_handler_mmsg.cpp
+++ host/lib/transport/super_recv_packet_handler_mmsg.cpp
@@ -359,11 +359,13 @@ void recv_packet_handler_mmsg::if_hdr_unpack(const uint32_t* packet_buff, vrt::i
 
     memcpy(modified_header, packet_buff, _HEADER_SIZE);
 
-    printf("_HEADER_SIZE: %lu\n", _HEADER_SIZE);
-    printf("modified_header[0]: %hhu\n", modified_header[0]);
-    printf("modified_header[1]: %hhu\n", modified_header[1]);
-    printf("modified_header[2]: %hhu\n", modified_header[2]);
-    printf("modified_header[3]: %hhu\n", modified_header[3]);
+    // Set has_tlr (has trailer) falg to false
+    // Our trailers are all 0 and get dropped during the network stage
+    modified_header[0] = modified_header[0] & 0b11111011;
+
+    uint_fast16_t vita_length = ( (uint_fast16_t) modified_header[3] << 0x100) + modified_header[2];
+
+    std::cout << "vita_length: " << vita_length << std::endl;
 
     vrt::if_hdr_unpack_be(packet_buff, if_packet_info);
 }
