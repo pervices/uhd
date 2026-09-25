5b8a763721323e9595c3ecb7dc0504171f76b619
Doug Wilson <doug.w@pervices.com>
2026-09-25 11:36:09 -0400

    debug msg

--- host/lib/transport/super_send_packet_handler_mmsg.cpp
+++ host/lib/transport/super_send_packet_handler_mmsg.cpp
@@ -42,6 +42,7 @@ send_packet_handler_mmsg::send_packet_handler_mmsg(const std::vector<size_t>& ch
     _streaming_locks(streaming_locks),
     _reprime_threshold((int64_t)(device_buffer_size * 0.1))
 {
+    printf("_DEVICE_PACKET_NSAMP_MULTIPLE: %lu\n", _DEVICE_PACKET_NSAMP_MULTIPLE);
     // Checks and warns the user if the preemption mode is suboptimal
     check_preemption("SEND_PACKET_HANDLER");
 
