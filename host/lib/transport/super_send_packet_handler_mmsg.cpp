ae46ae5779391fc1884b26896da7151d7b38dd9b
Doug Wilson <doug.w@pervices.com>
2026-09-25 12:52:49 -0400

    re-enable print and flush after it

--- host/lib/transport/super_send_packet_handler_mmsg.cpp
+++ host/lib/transport/super_send_packet_handler_mmsg.cpp
@@ -376,7 +376,8 @@ size_t send_packet_handler_mmsg::send(
 
         // If we are not mid reprime and the buffer level is below the target threshold
         if( device_time > ch_send_buffer_info_group[0].buffer_level_manager.peek_last_sob() && buffer_level  < 6000 /*_reprime_threshold*/) [[likely]] {
-            // printf("Reprime triggered after: %lu\n", samples_since_last_reprime);
+            printf("Reprime triggered after: %lu\n", samples_since_last_reprime);
+            fflush(stdout);
             samples_since_last_reprime = 0;
             // Time to start a new pseudo burst to recover
             // Reprime to 90% of the target buffer level
